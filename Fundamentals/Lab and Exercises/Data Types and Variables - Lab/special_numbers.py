de23c1598a3de54a5319c8cdba15e423abc91845
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-22 12:24:22 +0300

    Create special_numbers.py

---
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Lab/special_numbers.py
@@ -0,0 +1,3 @@
+# # Write a program that reads an integer n. Then, for all numbers in the range [1, n], prints the number and if it is special or not (True / False). 
+# A number is special when the sum of its digits is 5, 7, or 11.
+
