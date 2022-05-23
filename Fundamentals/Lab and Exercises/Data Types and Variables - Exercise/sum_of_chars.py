d77d810d149f0c0a5d8da70bb759ea8e0d4029ba
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:50:42 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/sum_of_chars.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/sum_of_chars.py
@@ -2,3 +2,12 @@
 # On the first line, you will receive N – the number of lines. On the following N lines – you will receive a letter per line. 
 # Print the total sum in the following format: "The sum equals: {total_sum}".
 # Note: n will be in the interval [1…20].
+
+lines = int(input())
+
+result = 0
+for _ in range (lines):
+    char = input()
+    result += ord(char)
+
+print(f"The sum equals: {result}")
