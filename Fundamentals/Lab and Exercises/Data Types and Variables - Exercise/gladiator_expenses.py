9bb321caa19376b08ca9e101666816becf659ff5
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:54:39 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/gladiator_expenses.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/gladiator_expenses.py
@@ -5,3 +5,23 @@
 # When both his sword and helmet are broken in the same lost fight, his shield also breaks.
 # Every second time his shield brakes, his armor also needs to be repaired. 
 # You will receive the price of each item in his equipment. Calculate his expenses for the year for renewing his equipment. 
+
+lost_fights = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+shield_counter = 0
+repairs = 0
+
+for fight in range(1, lost_fights + 1):
+    if fight % 2 == 0:
+        repairs += helmet_price
+    if fight % 3 == 0:
+        repairs += sword_price
+    if fight % 2 == 0 and fight % 3 == 0:
+        repairs += shield_price
+        shield_counter += 1
+        if shield_counter % 2 == 0:
+            repairs += armor_price
+print(f"Gladiator expenses: {repairs:.2f} aureus")
