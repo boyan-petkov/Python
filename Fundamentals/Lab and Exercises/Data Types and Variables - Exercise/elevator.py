eb6643cc5d50dd229e6b0b5b0f4063bf171dd876
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:44:03 +0300

    Create elevator.py

---
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/elevator.py
@@ -0,0 +1,2 @@
+# Calculate how many courses will be needed to elevate N persons by using an elevator with a capacity of P persons. 
+# The input holds two lines: the number of people N and the capacity P of the elevator.
