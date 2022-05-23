a7a9d06b0659dfe16defd40da22f8c5c4ac25f64
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:49:40 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/chars_to_string.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/chars_to_string.py
@@ -1,1 +1,5 @@
 # Write a function that receives 3 characters. Concatenate all the characters into one string and print it on the console.
+
+first, second, third = input(), input(), input()
+
+print(first + second + third)
