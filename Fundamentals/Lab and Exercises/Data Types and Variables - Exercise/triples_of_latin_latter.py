f96eb221f1160a54c3c315bd22a740c5b700fea3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:51:51 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/triples_of_latin_latter.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/triples_of_latin_latter.py
@@ -1,2 +1,8 @@
 # Write a program to read an integer N and print all triples of the first N small Latin letters, ordered alphabetically:
 
+num = int(input())
+
+for q in range(0, num):
+    for  w in range(0, num):
+        for e in range(0, num):
+            print(f"{chr(97 + q)}{chr(97 + w)}{chr(97 + e)}")
