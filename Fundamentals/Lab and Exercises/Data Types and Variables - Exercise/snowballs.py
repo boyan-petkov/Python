51830ad790def6b4461fcffa39a70e55c930a2e7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:47:51 +0300

    Create snowballs.py

---
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/snowballs.py
@@ -0,0 +1,10 @@
+# Tony and Andi love playing in the snow and having snowball fights, but they always argue about which makes the best snowballs. 
+# They have decided to involve you in their fray by writing a program that calculates snowball data and outputs the best snowball value.
+# You will receive N – an integer, the number of snowballs being made by Tony and Andi.
+# On the following lines, you will receive 3 inputs for each snowball:
+# •	The weight of the snowball (integer).
+# •	The time needed for the snowball to get to its target (integer).
+# •	The quality of the snowball (integer).
+# For each snowball, you must calculate its value by the following formula:
+# (snowball_weight / snowball_time) ** snowball_quality
+# In the end, you must print the highest calculated value of a snowball.
