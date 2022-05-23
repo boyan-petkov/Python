3956a75ba472e6edfea5ced4a63f8545eaa8c85e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:54:07 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/snowballs.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/snowballs.py
@@ -8,3 +8,20 @@
 # For each snowball, you must calculate its value by the following formula:
 # (snowball_weight / snowball_time) ** snowball_quality
 # In the end, you must print the highest calculated value of a snowball.
+
+snowballs = int(input())
+
+best_value, best_weight, best_time, best_quality = 0, 0, 0, 0
+
+for snowball in range(snowballs):
+    weight = int(input())
+    time_needed = int(input())
+    quality = int(input())
+    current_value = (weight / time_needed) ** quality
+    if current_value > best_value:
+        best_value = current_value
+        best_weight = weight
+        best_time = time_needed
+        best_quality = quality
+print(f"{best_weight} : {best_time} = {best_value:.0f} ({best_quality})")
+	
