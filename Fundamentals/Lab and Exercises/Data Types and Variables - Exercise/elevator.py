1f13310ac002cfd601ca0561fcdfff93753045ca
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:50:10 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/elevator.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/elevator.py
@@ -1,2 +1,12 @@
 # Calculate how many courses will be needed to elevate N persons by using an elevator with a capacity of P persons. 
 # The input holds two lines: the number of people N and the capacity P of the elevator.
+
+people = int(input())
+capacity = int(input())
+lifts = 0
+
+while people > 0:
+    people -= capacity
+    lifts += 1
+
+print(lifts)
