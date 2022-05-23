0192e17cc1d90d50db0c7b0c3c78430a720940c0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:52:39 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/water_overflow.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/water_overflow.py
@@ -2,3 +2,18 @@
 # which will follow. On the following n lines, you will receive liters of water (integers), which you should pour into your tank. 
 # If the capacity is not enough, 
 # print "Insufficient capacity!" and continue reading the next line. On the last line, print the liters in the tank.
+
+capacity = 255
+
+number = int(input())
+
+filled = 0
+
+for _ in range(number):
+    liters = int(input())
+    if filled + liters > capacity:
+        print(f"Insufficient capacity!")
+        continue
+    else:
+        filled += liters
+print(filled)
