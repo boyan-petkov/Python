85b33bae96b93ef257e912282a9b3be52a319b39
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-23 18:49:10 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/integer_operations.py
+++ Fundamentals/Lab and Exercises/Data Types and Variables - Exercise/integer_operations.py
@@ -1,2 +1,7 @@
 # Write a program that reads four integer numbers. It should add the first to the second number, integer divide the sum by the third number, 
 # and multiply the result by the fourth number. Print the final result.
+
+a, b, c, d, = int(input()), int(input()), int(input()), int(input()),
+
+result = ((a + b) // c) * d
+print(result)
