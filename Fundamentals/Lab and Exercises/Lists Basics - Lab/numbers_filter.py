6c7ed521a5cb26b615127e5254f277b479047e12
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-24 11:06:55 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Lists Basics - Lab/numbers_filter.py
+++ Fundamentals/Lab and Exercises/Lists Basics - Lab/numbers_filter.py
@@ -5,3 +5,33 @@
 # •	negative
 # •	positive
 # Filter all the numbers that fit in the category (0 counts as a positive and even). Finally, print the result.
+
+
+number = int(input())
+
+even = []
+odd = []
+negative = []
+positive = []
+decision = ""
+
+for _ in range(number):
+    current_num = int(input())
+    if current_num % 2 == 0 or current_num == 0:
+        even.append(current_num)
+    if current_num % 2 != 0:
+        odd.append(current_num)
+    if current_num >= 0:
+        positive.append(current_num)
+    if current_num < 0:
+        negative.append(current_num)
+command = input()
+if command == "even":
+    decision = even
+elif command == "odd":
+    decision = odd
+elif command == "positive":
+    decision = positive
+else:
+    decision = negative
+print(decision)
