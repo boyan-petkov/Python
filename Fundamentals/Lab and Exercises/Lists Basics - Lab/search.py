3c12a43cca4ebb0973d5e5b979d79214ab57a23c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-24 11:06:07 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Lists Basics - Lab/search.py
+++ Fundamentals/Lab and Exercises/Lists Basics - Lab/search.py
@@ -2,3 +2,16 @@
 # On the following n lines, you will be given some strings. 
 # You should add them to a list and print them. 
 # After that, you should filter out only the strings that include the given word and print that list too.
+
+number = int(input())
+
+key = input()
+non_filter = []
+filtered = []
+for _ in range(number):
+    current = input()
+    non_filter.append(current)
+    if key in current:
+        filtered.append(current)
+print(non_filter)
+print(filtered)
