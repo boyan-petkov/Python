a2fb3962d8fc29394f78cb04fe2d30fdb703378f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-24 11:02:40 +0300

    Create list_statistics.py

---
+++ Fundamentals/Lab and Exercises/Lists Basics - Lab/list_statistics.py
@@ -0,0 +1,7 @@
+# On the first line, you will receive a number n. On the following n lines, you will receive integers. 
+# You should create and print two lists:
+# •	One with all the positives (including 0) numbers
+# •	One with all the negatives numbers
+# Finally, print the following message: 
+# "Count of positives: {count_positives}
+# Sum of negatives: {sum_of_negatives}"
