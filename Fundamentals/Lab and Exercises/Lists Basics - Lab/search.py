88ec29c01d81fd44282d37134bb035f1bd17dd0f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-24 11:03:22 +0300

    Create search.py

---
+++ Fundamentals/Lab and Exercises/Lists Basics - Lab/search.py
@@ -0,0 +1,4 @@
+# On the first line, you will receive a number n. On the second line, you will receive a word. 
+# On the following n lines, you will be given some strings. 
+# You should add them to a list and print them. 
+# After that, you should filter out only the strings that include the given word and print that list too.
