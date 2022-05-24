138b4c475502335fd504ef5597d8cfb65ba1c65f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-24 11:04:57 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/Lists Basics - Lab/courses.py
+++ Fundamentals/Lab and Exercises/Lists Basics - Lab/courses.py
@@ -1,2 +1,10 @@
 # On the first line, you will receive a single number n. On the following n lines, you will receive names of courses. 
 # You should create a list of courses and print it.
+
+num = int(input())
+
+courses = []
+
+for _ in range(num):
+    courses.append(input())
+print(courses)
