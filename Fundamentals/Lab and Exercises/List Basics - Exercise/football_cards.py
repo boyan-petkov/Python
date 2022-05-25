782269a1cbd8dea92e3e9b35cb7f56080a8a9438
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-25 09:31:49 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/List Basics - Exercise/football_cards.py
+++ Fundamentals/Lab and Exercises/List Basics - Exercise/football_cards.py
@@ -9,3 +9,25 @@
 # You should print the count of remaining players on each team at the end of the game in the format: "Team A - {players_count}; Team B - {players_count}". 
 # If the referee terminated the game, print an additional line: "Game was terminated".
 # Note for the random tests: If a player who has already been sent off receives another card - ignore it.
+
+team_a = 11
+team_b = 11
+
+players_sent_off = []
+
+players = input().split()
+game_terminated = False
+for el in players:
+    team, player = el.split("-")
+    if el not in players_sent_off:
+        if team == "A":
+            team_a -= 1
+        if team == "B":
+            team_b -= 1
+        players_sent_off.append(el)
+        if team_a < 7 or team_b < 7:
+            game_terminated = True
+            break
+print(f"Team A - {team_a}; Team B - {team_b}")
+if game_terminated:
+    print(f"Game was terminated")
