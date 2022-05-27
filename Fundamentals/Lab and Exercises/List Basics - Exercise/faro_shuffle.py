00bf3b2186a14cfa2a503ea6375fb51ca4d5a495
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-27 11:03:19 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/List Basics - Exercise/faro_shuffle.py
+++ Fundamentals/Lab and Exercises/List Basics - Exercise/faro_shuffle.py
@@ -4,3 +4,20 @@
 # Write a program that receives a single string (cards separated by space) and on the second line receives a count of faro shuffles that should be made. 
 # Print the state of the deck after the shuffle.
 # Note: The length of the deck of cards will always be an even number.
+
+
+cards = input().split()
+shuffles = int(input())
+
+final_cards = []
+
+for shuffles in range(shuffles):
+    left_side = cards[:len(cards) // 2]
+    right_side = cards[len(cards) // 2:]
+    final_cards = []
+    for index in range(len(left_side)):
+        final_cards.append(left_side[index])
+        final_cards.append(right_side[index])
+        cards = final_cards
+print(final_cards)
+
