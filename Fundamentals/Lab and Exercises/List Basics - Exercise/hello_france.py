5cd721df2bcb4c300888eeff1b80addc094e02c7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-27 11:05:15 +0300

    Initial upload

--- Fundamentals/Lab and Exercises/List Basics - Exercise/hello_france.py
+++ Fundamentals/Lab and Exercises/List Basics - Exercise/hello_france.py
@@ -12,3 +12,43 @@
 # If you don't have enough money for it, you can't buy it. Buy as many items as you can.
 # Next, you should increase the price of each item you have successfully bought by 40% and then sell it. 
 # Calculate if the budget after selling all the items is enough for buying the train ticket.
+
+items = input().split("|")
+budget = int(input())
+output_list = []
+total_spend = 0
+total_gained = 0
+start_budget = budget
+
+for item in items:
+    current_item = item.split("->")
+    type = current_item[0]
+    price = float(current_item[1])
+    buy_now = False
+    if budget < price:
+        continue
+
+    if "Clothes" in type and price <= 50.00:
+        buy_now = True
+    elif "Shoes" in type and price <= 35.00:
+        buy_now = True
+    elif "Accessories" in type and price <= 20.50:
+        buy_now = True
+
+    if buy_now:
+        budget -= price
+        total_spend += price
+        sell_price = price + price * 0.40
+        total_gained += sell_price
+        output_list.append(sell_price)
+
+profit = (total_gained + budget) - start_budget
+for element in output_list:
+    print(f"{element:.2f}", end=" ")
+print()
+
+print(f"Profit: {profit:.2f}")
+if total_gained + budget >= 150:
+    print(f"Hello, France!")
+else:
+    print("Not enough money.")
