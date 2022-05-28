76afa5239592e9cdde148cf7ffcf859d08b2d0bd
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-28 20:05:43 +0300

    Create zeros_to_back.py

---
+++ Fundamentals/Lab and Exercises/Lists Basics - More Exercises/zeros_to_back.py
@@ -0,0 +1,3 @@
+# Write a program that receives a single string (integers separated by a comma and space ", "), finds all the zeros, 
+# and moves them to the back without messing up the other elements. 
+# Print the resulting integer list.
