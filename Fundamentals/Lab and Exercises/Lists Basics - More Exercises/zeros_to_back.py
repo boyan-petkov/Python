51075cb71a1f46c0be37b0e3c83da59bfa89d623
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-28 20:09:01 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Lists Basics - More Exercises/zeros_to_back.py
+++ Fundamentals/Lab and Exercises/Lists Basics - More Exercises/zeros_to_back.py
@@ -1,3 +1,16 @@
 # Write a program that receives a single string (integers separated by a comma and space ", "), finds all the zeros, 
 # and moves them to the back without messing up the other elements. 
 # Print the resulting integer list.
+
+
+integer_list = [int(x) for x in input().split(", ")]
+
+count = 0
+while 0 in integer_list:
+    integer_list.remove(0)
+    count += 1
+for zeros in range(count):
+    integer_list.append(0)
+
+print(integer_list)
+
