5bb1b0e25ae40a2f4347d9fd01b22f7028f4656a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-28 20:10:06 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Lists Basics - More Exercises/car_race.py
+++ Fundamentals/Lab and Exercises/Lists Basics - More Exercises/car_race.py
@@ -7,3 +7,29 @@
 # The number of elements in the sequence will always be odd.
 # Print the result in the following format "The winner is {left/right} with total time: {total_time}".
 # The time should be formatted to the first decimal point.
+
+race = [int(i) for i in input().split(' ')]
+finish = len(race) // 2
+f_car = race[0:finish]
+s_car = race[-1:finish:-1]
+ 
+score_f_car = 0
+score_s_car = 0
+ 
+for _ in f_car:
+    score_f_car += _
+    if _ == 0:
+        score_f_car *= 0.8
+ 
+ 
+ 
+for _ in s_car:
+    score_s_car += _
+    if _ == 0:
+        score_s_car *= 0.8
+ 
+ 
+if score_f_car > score_s_car:
+    print(f"The winner is right with total time: {score_s_car:.1f}")
+else:
+    print(f"The winner is left with total time: {score_f_car:.1f}")
