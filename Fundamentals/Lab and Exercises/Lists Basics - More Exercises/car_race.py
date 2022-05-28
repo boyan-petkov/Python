e8839d46a25a40f10a4f8b905b56e287fb82434c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-28 20:07:08 +0300

    Create car_race.py

---
+++ Fundamentals/Lab and Exercises/Lists Basics - More Exercises/car_race.py
@@ -0,0 +1,9 @@
+# Write a program that announces the winner of a car race. 
+# You will receive a sequence of numbers. Each number represents the time needed for the car to pass through that step (the index). 
+# There will be two cars. The first one starts from the left side, and the other one starts from the right side. 
+# The middle index of the sequence is the finish line. 
+# Calculate the total time each racer needs to reach the finish line and print the winner with his total time (the racer with less time). 
+# If you have a zero in the list, you should reduce the racer's time that reached it by 20% (from his current time).
+# The number of elements in the sequence will always be odd.
+# Print the result in the following format "The winner is {left/right} with total time: {total_time}".
+# The time should be formatted to the first decimal point.
