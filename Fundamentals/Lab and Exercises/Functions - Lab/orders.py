7036dc2f251ecaa51603945a788b76878d576b81
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-30 17:01:39 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Lab/orders.py
+++ Fundamentals/Lab and Exercises/Functions - Lab/orders.py
@@ -7,3 +7,22 @@
 # •	snacks - 2.00
 
 # Print the result formatted to the second decimal place.
+
+
+def order(product, number):
+    if product == "coffee":
+        price = 1.50
+        return price * number
+    elif product == "coke":
+        price = 1.40
+        return price * number
+    elif product == "water":
+        price = 1.00
+        return price * number
+    elif product == "snacks":
+        price = 2.00
+        return price * number
+
+
+current_product, current_number = input(), int(input())
+print(f"{order(current_product, current_number):.2f}")
