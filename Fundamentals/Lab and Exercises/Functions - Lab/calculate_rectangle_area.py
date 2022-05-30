d82127bea6b2db0c8083afa1caf9426a7bce3645
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-30 17:02:07 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Lab/calculate_rectangle_area.py
+++ Fundamentals/Lab and Exercises/Functions - Lab/calculate_rectangle_area.py
@@ -1,1 +1,10 @@
 # Create a function that calculates and returns the area of a rectangle by given width and height. Print the result on the console.
+
+
+def area_rectangle(w, h):
+    area = w * h
+    return area
+
+
+width, height = int(input()), int(input())
+print(area_rectangle(width, height))
