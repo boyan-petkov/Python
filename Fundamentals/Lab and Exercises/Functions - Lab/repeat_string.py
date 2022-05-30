ef30226b17ca36d33fc02198774f411ed3ff9c77
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-30 17:01:14 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Lab/repeat_string.py
+++ Fundamentals/Lab and Exercises/Functions - Lab/repeat_string.py
@@ -1,1 +1,8 @@
 # Write a function that receives a string and a counter n. The function should return a new string – the re
+
+
+repeat_string = lambda a, b: a * b
+
+string, count = input(), int(input())
+
+print(repeat_string(string, count))
