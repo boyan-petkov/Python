2e903809c9acabd6435192053424fa324176cd4f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-30 17:02:30 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Lab/rounding.py
+++ Fundamentals/Lab and Exercises/Functions - Lab/rounding.py
@@ -1,1 +1,12 @@
 # Write a program that rounds all the given numbers, separated by a single space, and prints the result as a list. Use round().
+
+
+def round_nums():
+    nums = [float(num) for num in input().split()]
+    final = []
+    for el in nums:
+        final.append(round(el))
+    return final
+
+
+print(round_nums())
