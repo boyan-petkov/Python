be5437155604b1cf2497f781a3e048e4c0d3ef6b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-30 17:00:49 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Lab/calculations.py
+++ Fundamentals/Lab and Exercises/Functions - Lab/calculations.py
@@ -3,3 +3,18 @@
 # The input comes as three parameters – an operator as a string and two integer numbers. 
 # The operator can be one of the following:  
 # "multiply", "divide", "add", "subtract". 
+
+def calculate_numbers(operator, num1, num2):
+    if operator == "multiply":
+        return num1 * num2
+    elif operator == "divide":
+        return num1 / num2
+    elif operator == "add":
+        return num1 + num2
+    elif operator == "subtract":
+        return num1 - num2
+
+
+input_operator, number_1, number_2 = input(), int(input()), int(input())
+
+print(int(calculate_numbers(input_operator, number_1, number_2)))
