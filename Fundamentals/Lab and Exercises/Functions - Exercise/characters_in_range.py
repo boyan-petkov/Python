0fa5e9e077805ceadd95252d6fa6d4a8b474b32d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-31 10:31:31 +0300

    Create characters_in_range.py

---
+++ Fundamentals/Lab and Exercises/Functions - Exercise/characters_in_range.py
@@ -0,0 +1,3 @@
+# Write a function that receives two characters and returns a single string with all the characters 
+# in between them (according to the ASCII code), separated by a single space. 
+# Print the result on the console.
