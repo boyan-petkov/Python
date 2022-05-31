580d8dae1f34b74a88029ad5b677e56afbc86e4d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-05-31 10:30:50 +0300

    Create add_and_subtract.py

---
+++ Fundamentals/Lab and Exercises/Functions - Exercise/add_and_subtract.py
@@ -0,0 +1,6 @@
+# You will receive three integer numbers. 
+# Write functions named:
+# •	sum_numbers() that returns the sum of the first two integers
+# •	subtract() that returns the difference between the returned result of the first function and the third integer
+# Wrap the two functions in a function named add_and_subtract() which will receive the three numbers as parameters. 
+# Print the result of the subtract() function on the console.
