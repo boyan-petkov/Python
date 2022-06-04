feb32b7117045c32e76d25aeea1e4e6deaeb395a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-04 13:31:38 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Exercise/odd_and_even.py
+++ Fundamentals/Lab and Exercises/Functions - Exercise/odd_and_even.py
@@ -2,3 +2,17 @@
 # The result should be returned as a single string in the format: 
 # "Odd sum = {sum_of_odd_digits}, Even sum = {sum_of_even_digits}"
 # Print the result of the function on the console.
+
+def sum_even_and_odd(number: str):
+    even_sum = 0
+    odd_sum = 0
+    for i in range(len(number)):
+        if int(number[i]) % 2 == 0:
+            even_sum += int(number[i])
+        else:
+            odd_sum += int(number[i])
+    print(f"Odd sum = {odd_sum}, Even sum = {even_sum}")
+
+
+num = input()
+sum_even_and_odd(num)
