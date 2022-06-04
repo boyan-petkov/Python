3890824b29c1aec9e4ac364f26a7bb34fadeb4c1
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-04 13:30:29 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Exercise/add_and_subtract.py
+++ Fundamentals/Lab and Exercises/Functions - Exercise/add_and_subtract.py
@@ -4,3 +4,17 @@
 # •	subtract() that returns the difference between the returned result of the first function and the third integer
 # Wrap the two functions in a function named add_and_subtract() which will receive the three numbers as parameters. 
 # Print the result of the subtract() function on the console.
+
+
+def add_and_subtract(first_num, second_num, third_num):
+    def sum_numbers():
+        return first_num + second_num
+
+    def subtract():
+        return sum_numbers() - third_num
+
+    return subtract()
+
+
+num1, num2, num3 = int(input()), int(input()), int(input()),
+print(add_and_subtract(num1, num2, num3))
