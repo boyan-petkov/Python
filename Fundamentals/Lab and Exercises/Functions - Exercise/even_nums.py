476683b4074ef40263efdd071e3b7af1a4f81319
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-04 13:32:17 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Exercise/even_nums.py
+++ Fundamentals/Lab and Exercises/Functions - Exercise/even_nums.py
@@ -1,2 +1,14 @@
 # Write a program that receives a sequence of numbers (integers) separated by a single space. 
 # It should print a list of only the even numbers. Use filter().
+
+
+def even_only(x):
+    if x % 2 == 0:
+        return True
+    return False
+
+
+nums = [int(el) for el in input().split()]
+
+even = filter(even_only, nums)
+print(list(even))
