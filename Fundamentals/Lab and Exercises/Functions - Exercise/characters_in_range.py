bd9abe27d74b4b931adb4debf8cb9a27cccd995f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-04 13:31:01 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Exercise/characters_in_range.py
+++ Fundamentals/Lab and Exercises/Functions - Exercise/characters_in_range.py
@@ -1,3 +1,13 @@
 # Write a function that receives two characters and returns a single string with all the characters 
 # in between them (according to the ASCII code), separated by a single space. 
 # Print the result on the console.
+
+
+def chars(start, end):
+    for el in range(ord(start) + 1, ord(end)):
+        print(chr(el), end=" ")
+
+
+first = input()
+last = input()
+chars(first, last)
