5f8ec7129a6b767d29c5433c6d881e15be5632d0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-04 13:32:55 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Exercise/sort.py
+++ Fundamentals/Lab and Exercises/Functions - Exercise/sort.py
@@ -1,2 +1,11 @@
-Write a program that receives a sequence of numbers (integers) separated by a single space. 
-It should print a list of only the even numbers. Use filter().
+# Write a program that receives a sequence of numbers (integers) separated by a single space. 
+# It should print a list of only the even numbers. Use filter().
+
+
+def sorting(nums):
+    nums = sorted(nums)
+    return nums
+
+
+numbers = [int(el) for el in input().split()]
+print(sorting(numbers))
