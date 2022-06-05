9d8022ccbf482f14420565686c81ed64eaea5651
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-05 10:02:39 +0300

    Delete factorial_division.py

--- Fundamentals/Lab and Exercises/Functions - Exercise/factorial_division.py
+++
@@ -1,2 +0,0 @@
-# Write a function that receives two integer numbers. Calculate the factorial of each number. 
-# Divide the first result by the second and print the division formatted to the second decimal point.
