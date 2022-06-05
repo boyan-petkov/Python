7781bc52b308765c480fc8aea79462bd650a775f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-05 09:58:35 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Functions - Exercise/min_and_max_sum.py
+++ Fundamentals/Lab and Exercises/Functions - Exercise/min_and_max_sum.py
@@ -6,3 +6,14 @@
 # •	On the second line: "The maximum number is {maximum number}"
 # •	On the third line: "The sum number is: {sum of all numbers}"
 
+def my_function(nums):
+    minimum = min((nums))
+    maximum = max((nums))
+    total_sum = sum((nums))
+    print(f"The minimum number is {minimum}")
+    print(f"The maximum number is {maximum}")
+    print(f"The sum number is: {total_sum}")
+
+
+numbers = [int(el) for el in input().split()]
+my_function(numbers)
