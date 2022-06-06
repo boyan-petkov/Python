c600c604c8f4a52e09f45bb167befad18cd56f90
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-06 14:53:29 +0300

    Create to_do_list.py

---
+++ Fundamentals/Lab and Exercises/Lists Advanced - Lab/to_do_list.py
@@ -0,0 +1,3 @@
+# You will be receiving to-do notes until you get the command "End". The notes will be in the format: "{importance}-{note}". 
+# Return a list containing all to-do notes sorted by importance in ascending order. 
+# The importance value will always be an integer between 1 and 10 (inclusive).
