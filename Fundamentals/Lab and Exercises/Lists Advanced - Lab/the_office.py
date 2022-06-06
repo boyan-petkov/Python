1f888518519c372b5189c4a07c54581389f3edf1
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-06 14:55:02 +0300

    Create the_office.py

---
+++ Fundamentals/Lab and Exercises/Lists Advanced - Lab/the_office.py
@@ -0,0 +1,10 @@
+# You will receive two lines of input: 
+# •	a list of employees' happiness as a string of numbers separated by a single space 
+# •	a happiness improvement factor (single number).
+# Your task is to find out if the employees are generally happy in their office. 
+# First, multiply each employee's happiness by the factor.
+# Then, print one of the following lines:
+# •	If half or more of the employees have happiness greater than or equal to the average:
+# "Score: {happy_count}/{total_count}. Employees are happy!"
+# •	Otherwise:
+# "Score: {happy_count}/{total_count}. Employees are not happy!"
