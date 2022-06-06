763d2366140ce03e99a2b0ac0832898ae80b1e39
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-06 14:52:33 +0300

    Create no_vowels.py

---
+++ Fundamentals/Lab and Exercises/Lists Advanced - Lab/no_vowels.py
@@ -0,0 +1,2 @@
+# Using comprehension, write a program that receives a text and removes all its vowels, case insensitive. 
+# Print the new text string after removing the vowels. The vowels that should be considered are 'a', 'o', 'u', 'e', 'i'.
