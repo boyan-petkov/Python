358ac71a6db2847920013d8f1aa9075d57948d7d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-07 13:35:43 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Lists Advanced - Lab/to_do_list.py
+++ Fundamentals/Lab and Exercises/Lists Advanced - Lab/to_do_list.py
@@ -1,3 +1,15 @@
 # You will be receiving to-do notes until you get the command "End". The notes will be in the format: "{importance}-{note}". 
 # Return a list containing all to-do notes sorted by importance in ascending order. 
 # The importance value will always be an integer between 1 and 10 (inclusive).
+
+to_do = ["" for i in range(11)]
+
+command = input()
+while not command == "End":
+    command = command.split("-")
+    priority, note = int(command[0]), (command[1])
+    to_do[priority] = note
+    command = input()
+
+final = [el for el in to_do if not el == ""]
+print(final)
