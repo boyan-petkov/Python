e8ed0eba6e17e51156bf19c9a25d02798d0115f3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-07 13:36:24 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Lists Advanced - Lab/palindrome_strings.py
+++ Fundamentals/Lab and Exercises/Lists Advanced - Lab/palindrome_strings.py
@@ -1,3 +1,18 @@
 # On the first line, you will receive words separated by a single space. On the second line, you will receive a palindrome. 
 # First, you should print a list containing all the found palindromes in the sequence. 
 # Then, you should print the number of occurrences of the given palindrome in the format: "Found palindrome {number} times".
+
+words = [el for el in input().split()]
+
+palindromes = [el for el in words if el == el[::-1]]
+
+searched = input()
+
+count = 0
+
+for i in range(len(palindromes)):
+    if searched == palindromes[i]:
+        count += 1
+
+print(palindromes)
+print(f"Found palindrome {count} times")
