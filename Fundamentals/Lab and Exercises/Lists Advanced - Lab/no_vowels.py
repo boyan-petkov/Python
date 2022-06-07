9e5d6716ba397971e84f8e57bcd37881819f8df1
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-07 13:34:31 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Lists Advanced - Lab/no_vowels.py
+++ Fundamentals/Lab and Exercises/Lists Advanced - Lab/no_vowels.py
@@ -1,2 +1,9 @@
 # Using comprehension, write a program that receives a text and removes all its vowels, case insensitive. 
 # Print the new text string after removing the vowels. The vowels that should be considered are 'a', 'o', 'u', 'e', 'i'.
+
+vowels = ['a', 'o', 'u', 'e', 'i', 'A', 'O', 'U', 'E', 'I']
+
+text = input()
+string = [el for el in text if el not in vowels]
+
+print("".join(string))
