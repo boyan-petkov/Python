35e9e7fe9e7eae0ae497264db0d03cdc654dbf6b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-07 13:37:20 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Lists Advanced - Lab/even_numbers.py
+++ Fundamentals/Lab and Exercises/Lists Advanced - Lab/even_numbers.py
@@ -1,1 +1,10 @@
 # Write a program that reads a single string with numbers separated by comma and space ", ". Print the indices of all even numbers.
+
+nums = [int(el) for el in input().split(", ")]
+
+indexes = []
+for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            indexes.append(i)
+
+print(indexes)
