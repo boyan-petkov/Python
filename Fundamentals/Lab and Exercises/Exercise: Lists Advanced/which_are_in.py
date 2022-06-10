366895435013ad0dfdd0be81a2487596f1660243
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-10 09:48:47 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Exercise: Lists Advanced/which_are_in.py
+++ Fundamentals/Lab and Exercises/Exercise: Lists Advanced/which_are_in.py
@@ -1,2 +1,13 @@
 # You will be given two sequences of strings, separated by ", ". 
 # Print a new list containing only the strings from the first input line, which are substrings of any string in the second input line.
+
+first_string = input().split(", ")
+second = input().split(", ")
+
+final = []
+for el in first_string:
+    for word in second:
+        if el in word:
+            if el not in final:
+                final.append(el)
+print(final)
