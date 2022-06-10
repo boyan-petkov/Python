5f79ee98878714fdeb1c9cd9ecf5a3fc2b6bfe71
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-10 09:49:41 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Exercise: Lists Advanced/world_filter.py
+++ Fundamentals/Lab and Exercises/Exercise: Lists Advanced/world_filter.py
@@ -1,2 +1,8 @@
 # Using comprehension, write a program that receives some text, separated by space, and take only those words whose length is even.
 # Print each word on a new line.
+
+words = input().split()
+
+filtered = [word for word in words if len(word) % 2 == 0]
+
+print("\n".join(filtered))
