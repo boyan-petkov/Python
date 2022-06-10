9601a71bbe2139abb20e3638358555aadbaa0524
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-10 09:49:15 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Exercise: Lists Advanced/next_version.py
+++ Fundamentals/Lab and Exercises/Exercise: Lists Advanced/next_version.py
@@ -2,3 +2,18 @@
 # You will be given a string representing the version of your software in the format: "{n1}.{n2}.{n3}". Your task is to print the next version. 
 #   For example, if the current version is "1.3.4", the next version will be "1.3.5". 
 # The only rule is that the numbers cannot be greater than 9. If it happens, set the current number to 0 and increase the previous number. 
+
+
+current_version = list(map(int, input().split(".")))
+
+if current_version[2] < 9:
+    current_version[2] += 1
+else:
+    current_version[2] = 0
+    if current_version[1] < 9:
+        current_version[1] += 1
+    else:
+        current_version[1] = 0
+        current_version[0] += 1
+current_version = list(map(str, current_version))
+print(".".join(current_version))
