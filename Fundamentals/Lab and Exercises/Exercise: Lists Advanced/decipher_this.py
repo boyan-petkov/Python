a318a045876da9dd8eb6f1404c33841aa194e19c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-11 18:21:17 +0300

    Create decipher_this.py

---
+++ Fundamentals/Lab and Exercises/Exercise: Lists Advanced/decipher_this.py
@@ -0,0 +1,4 @@
+# You are given a secret message you should decipher. To do that, you need to know that in each word:
+# •	the second and the last letter are switched (e.g., Holle means Hello)
+# •	the first letter is replaced by its character code (e.g., 72 means H)
+
