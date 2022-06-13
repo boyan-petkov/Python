6e06bee2145f9dd99b584a5c37b9f3e0723ca908
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-13 22:20:42 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Exercise: Lists Advanced/decipher_this.py
+++ Fundamentals/Lab and Exercises/Exercise: Lists Advanced/decipher_this.py
@@ -2,3 +2,16 @@
 # •	the second and the last letter are switched (e.g., Holle means Hello)
 # •	the first letter is replaced by its character code (e.g., 72 means H)
 
+words = input().split()
+final = []
+
+for word in words:
+    current = list(word)
+    character = [el for el in current if el.isdigit()]
+    filtered = [el for el in current if el not in character]
+    needed = chr(int("".join(character)))
+    filtered.insert(0, needed)
+    filtered[1], filtered[-1] = filtered[-1], filtered[1]
+    final.append("".join(filtered))
+
+print(" ".join(final))
