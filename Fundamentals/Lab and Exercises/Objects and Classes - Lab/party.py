19f64d6935b4f5a99b1e9e0fa9688fd7aca369c4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-14 16:04:41 +0300

    Create party.py

---
+++ Fundamentals/Lab and Exercises/Objects and Classes - Lab/party.py
@@ -0,0 +1,18 @@
+# Create a class Party that only has an attribute people – empty list. The __init__ method should not accept any parameters. 
+# You will be given names of people (on separate lines) until you receive the command "End". Use the created class to solve this problem. 
+# After you receive the "End" command, print 2 lines:
+# •	"Going: {people}" - the people should be separated by comma and space ", ".
+# •	"Total: {total_people_going}"
+# Note: submit all of your code, including the class
+
+# Input
+# Peter
+# John
+# Katy
+
+# Input2
+# Sam
+# Eddy
+# Edd
+# Kris
+# End
