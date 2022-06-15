e3229c00665069d0069ba5cb2dada764c6b0396b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-15 12:12:06 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Objects and Classes - Lab/email.py
+++ Fundamentals/Lab and Exercises/Objects and Classes - Lab/email.py
@@ -8,6 +8,40 @@
 # Call the send() method for the given indices of emails. For each email, call the get_info() method.
 
 
+class Email:
+    def __init__(self, sender, receiver, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+        self.is_sent = False
+
+    def send(self):
+        self.is_sent =True
+
+    def get_info(self):
+        return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
+
+emails = []
+
+cmd = input()
+
+while not cmd == "Stop":
+    send, rec, cont = cmd.split()
+    email = Email(send, rec, cont)
+    emails.append(email)
+    cmd = input()
+
+send_emails = list(map(lambda x: int(x), input().split(", ")))
+
+for el in send_emails:
+    emails[el].send()
+
+for email in emails:
+    print(email.get_info())
+
+
+
+
 # Input
 # Peter John Hi,John
 # John Peter Hi,Peter!
