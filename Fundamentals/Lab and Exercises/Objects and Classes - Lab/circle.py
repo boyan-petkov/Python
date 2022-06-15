20f395108b5ec4768646a9ad5c496d593ff4765d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-15 12:14:11 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Objects and Classes - Lab/circle.py
+++ Fundamentals/Lab and Exercises/Objects and Classes - Lab/circle.py
@@ -5,3 +5,25 @@
 # •	calculate_area_of_sector(angle) - gives the central angle in degrees, returns the area that fills the sector
 # Notes: Search the formulas on the internet. Name your methods and variables exactly as in the description! Submit only the class.
 #   Test your class before submitting it!
+
+class Circle:
+    __pi = 3.14
+
+    def __init__(self, diameter):
+        self.diameter = diameter
+
+    def calculate_circumference(self):
+        circumference = self.diameter * self.__pi
+        return circumference
+
+    def calculate_area(self):
+        r = self.diameter / 2
+        area = self.__pi * (r ** 2)
+        return area
+
+    def calculate_area_of_sector(self, angle):
+        r = self.diameter / 2
+        sector_area = (angle / 360) * self.__pi * (r ** 2)
+        return sector_area
+
+
