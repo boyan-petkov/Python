36306c72a4f22d8a80b2f794d5e7a129ae52194b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-18 09:53:08 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Objects and Classes - Exericse/storage.py
+++ Fundamentals/Lab and Exercises/Objects and Classes - Exericse/storage.py
@@ -4,7 +4,18 @@
 # •	add_product(product: str) - adds the product in the storage if there is enough space for it
 # •	get_products() - returns the storage list
 
+class Storage:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = []
 
+    def add_product(self, product):
+        if len(self.storage) < self.capacity:
+            self.storage.append(product)
+
+    def get_products(self):
+        return self.storage
+      
 storage = Storage(4)
 storage.add_product("apple")
 storage.add_product("banana")
