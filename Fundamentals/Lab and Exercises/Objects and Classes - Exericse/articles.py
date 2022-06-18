01f18bb49e55b739daa0790f577da8995cc8640a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-18 09:56:49 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Objects and Classes - Exericse/articles.py
+++ Fundamentals/Lab and Exercises/Objects and Classes - Exericse/articles.py
@@ -5,6 +5,28 @@
 # •	rename(new_title: str) - changes the old title with the new one
 # •	__repr__() - returns the following string "{title} - {content}: {author}"
 
+
+
+class Article:
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+
+    def edit(self, new_content):
+        self.content = new_content
+
+    def change_author(self, new_author):
+        self.author = new_author
+
+    def rename(self, new_title):
+        self.title = new_title
+
+    def __repr__(self):
+        return f"{self.title} - {self.content}: {self.author}"
+
+    
+    
 article = Article(
     "Highest Recorded Temperature", 
     "Temperatures across Europe are unprecedented, according to scientists.", 
