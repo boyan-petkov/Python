5a52ebb73c85c900faf1c4cb4134da02169e33ac
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-18 09:53:52 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Objects and Classes - Exericse/weapon.py
+++ Fundamentals/Lab and Exercises/Objects and Classes - Exericse/weapon.py
@@ -7,6 +7,20 @@
 # o	Returns "Remaining bullets: {amount_of_bullets}"
 
 
+
+class Weapon:
+    def __init__(self, bullets):
+        self.bullets = int(bullets)
+
+    def shoot(self):
+        if self.bullets > 0:
+            self.bullets -= 1
+            return "shooting..."
+        return "no bullets left"
+
+    def __repr__(self):
+        return f"Remaining bullets: {self.bullets}"
+
 weapon = Weapon(5)
 print(weapon.shoot())
 print(weapon.shoot())
