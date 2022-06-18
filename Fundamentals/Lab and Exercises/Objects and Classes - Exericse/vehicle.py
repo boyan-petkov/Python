6254a89d0990f9af569ff60cfb1545ba3de4e55d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-18 09:57:20 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Objects and Classes - Exericse/vehicle.py
+++ Fundamentals/Lab and Exercises/Objects and Classes - Exericse/vehicle.py
@@ -11,6 +11,37 @@
 # o	If the vehicle has an owner, returns: "{model} {type} is owned by: {owner}". 
 # o	Otherwise, return: "{model} {type} is on sale: {price}"
 
+
+class Vehicle:
+    def __init__(self, type, model, price):
+        self.type = type
+        self.model = model
+        self.price = price
+        self.owner = None
+
+    def buy(self, money, owner):
+        if money >= self.price and self.owner == None:
+            self.owner = owner
+            return f"Successfully bought a {self.type}. Change: {(self.price - money):.2f}"
+        elif money < self.price:
+            return "Sorry, not enough money"
+        else:
+            return "Car already sold"
+
+    def sell(self):
+        if not self.owner == None:
+            self.owner = None
+        else:
+            return "Vehicle has no owner"
+
+    def __repr__(self):
+        if not self.owner == None:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        else:
+            return f"{self.model} {self.type} is on sale: {self.price}"
+
+
+
 vehicle_type = "car"
 model = "BMW"
 price = 30000
