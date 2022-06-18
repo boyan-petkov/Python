88d5a287ac5f877e9458a29d74a1c6c2b8d8183e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-18 09:54:24 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Objects and Classes - Exericse/catalogue.py
+++ Fundamentals/Lab and Exercises/Objects and Classes - Exericse/catalogue.py
@@ -11,6 +11,26 @@
 # The items should be sorted alphabetically in ascending order.
 
 
+
+class Catalogue:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def get_by_letter(self, first_letter):
+        return [word for word in self.products if word[0] == first_letter]
+
+    def __repr__(self):
+        result = f"Items in the {self.name} catalogue:\n"
+        sorted_products = sorted(self.products)
+        result += "\n".join(sorted_products)
+        # for words in sorted_products:
+        #     result += words + "\n"
+        return result
+
 catalogue = Catalogue("Furniture")
 catalogue.add_product("Sofa")
 catalogue.add_product("Mirror")
