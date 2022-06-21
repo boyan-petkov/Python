dbc348ab71c236a1023250a1e81cbb61b45d6ba2
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-21 11:56:34 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Lab/ascii_values.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Lab/ascii_values.py
@@ -1,3 +1,14 @@
 # Write a program that receives a list of characters separated by ", ". 
 # It should create a dictionary with each character as a key and its ASCII value as a value. 
 # Try solving that problem using comprehension.
+
+
+characters = input().split(", ")
+
+final = {chr: ord(chr) for chr in characters}
+
+# for chr in characters:
+#     asci = ord(chr)
+#     final[chr] = asci
+
+print(final)
