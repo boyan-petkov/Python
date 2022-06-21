1ced4a848e701ea06616a01609bc5192182f6df0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-21 11:55:03 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Lab/stock.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Lab/stock.py
@@ -4,3 +4,21 @@
 # On the following line, you will be given products to search for. Check for each product. You have 2 possibilities:
 # •	If you have the product, print "We have {quantity} of {product} left".
 # •	Otherwise, print "Sorry, we don't have {product}".
+
+
+products = input().split()
+
+stock = {}
+
+for i in range(0, len(products), 2):
+    key = products[i]
+    value = products[i + 1]
+    stock[key] = value
+
+searched = input().split()
+
+for el in searched:
+    if el in stock:
+        print(f"We have {stock[el]} of {el} left")
+    else:
+        print(f"Sorry, we don't have {el}")
