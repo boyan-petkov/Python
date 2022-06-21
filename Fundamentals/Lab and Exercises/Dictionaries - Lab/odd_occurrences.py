582031f474708c2a6ded5b23dabccc28b0196bb8
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-21 11:57:08 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Lab/odd_occurrences.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Lab/odd_occurrences.py
@@ -1,3 +1,19 @@
-Write a program that extracts all elements from a given sequence of words present in it an odd number of times (case-insensitive).
-•	Words are given on a single line, space-separated.
-•	Print the result elements in lowercase, in their order of appearance.
+# Write a program that extracts all elements from a given sequence of words present in it an odd number of times (case-insensitive).
+# •	Words are given on a single line, space-separated.
+# •	Print the result elements in lowercase, in their order of appearance.
+
+
+elements = input().split()
+
+initiate = dict()
+
+for el in elements:
+    el = el.lower()
+    if el not in initiate:
+        initiate[el] = 1
+    else:
+        initiate[el] += 1
+
+for key, value in initiate.items():
+    if value % 2 != 0:
+        print(key, end=" ")
