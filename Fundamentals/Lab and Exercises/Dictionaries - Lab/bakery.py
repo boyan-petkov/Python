a8cf0ea0c4cc028aa03494257c0c6964a8b181ad
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-21 11:54:27 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Lab/bakery.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Lab/bakery.py
@@ -2,3 +2,15 @@
 # You will receive a single line containing some food (keys) and quantities (values). 
 # They will be separated by a single space (the first element is the key, the second – the value, and so on). 
 # Create a dictionary with all the keys and values and print it on the console.
+
+
+stock = input().split()
+
+dict = {}
+
+for i in range (0, len(stock), 2):
+    key = stock[i]
+    value = stock[i + 1]
+    dict[key] = int(value)
+
+print(dict)
