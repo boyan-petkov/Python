1817512e5f544123a52a0abbe05cedc383f18a49
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-22 11:11:24 +0300

    Create courses.py

---
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/courses.py
@@ -0,0 +1,4 @@
+# Write a program that keeps the information about courses. Each course has a name and registered students.
+# You will be receiving a course name and a student name until you receive the command "end". 
+# You should register each user into the corresponding course. If the given course does not exist, add it. 
+# When you receive the command "end", print the courses with their names and total registered users. For each course, print the registered users.
