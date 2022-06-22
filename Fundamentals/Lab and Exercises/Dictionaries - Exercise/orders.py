8f99514862189df421627d188c66a3d2a5a6230e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-22 10:44:50 +0300

    Create orders.py

---
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/orders.py
@@ -0,0 +1,5 @@
+# Write a program that keeps the information about products and their prices. Each product has a name, a price, and a quantity:
+# •	If the product doesn't exist yet, add it with its starting quantity.
+# •	If you receive a product, which already exists, increases its quantity by the input quantity and if its price is different, replace the price as well.
+# You will receive products' names, prices, and quantities on new lines. Until you receive the command "buy", keep adding items. 
+# Finally, print all items with their names and the total price of each product. 
