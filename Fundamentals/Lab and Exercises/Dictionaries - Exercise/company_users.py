5d44bb90b29c154d10c4e5145ad32f291dfdf838
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-22 11:12:45 +0300

    Create company_users.py

---
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/company_users.py
@@ -0,0 +1,9 @@
+# Write a program that keeps the information about companies and their employees. 
+# You will be receiving company names and an employees' id until you receive the command "End" command.
+# Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
+# Print the company name and each employee's id in the following format:
+# "{company_name}
+# -- {id1}
+# -- {id2}
+# …
+# -- {idN}"
