9b08e9dd169f4acb65d89e172d884594fc9d3e00
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-22 11:10:09 +0300

    Create softuni_parking.py

---
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/softuni_parking.py
@@ -0,0 +1,16 @@
+# SoftUni just got a new fancy parking lot. It even has online parking validation, except the online service doesn't work. 
+# It can only receive users' data, but it doesn't know what to do with it. Good thing you're on the dev team and know how to fix it, right?
+# Write a program, which validates a parking place - users can register to enter the park and unregister to leave.
+# The program receives 2 types of commands:
+# •	"register {username} {license_plate_number}":
+# o	The system only supports one car per user at the moment, so if a user tries to register another license plate using the same username, the system should print:
+# "ERROR: already registered with plate number {license_plate_number}"
+# o	If the check above passes successfully, the user should be registered, so the system should print:
+#  "{username} registered {license_plate_number} successfully"
+# •	"unregister {username}":
+# o	If the user is not present in the database, the system should print:
+# "ERROR: user {username} not found"
+# o	If the check above passes successfully, the system should print:
+# "{username} unregistered successfully"
+# After you execute all of the commands, print all the currently registered users and their license plates in the format: 
+# •	"{username} => {license_plate_number}"
