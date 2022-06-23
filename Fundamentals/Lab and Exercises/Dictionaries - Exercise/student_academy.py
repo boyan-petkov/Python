2baeff7594203609037cf742ea3d60562265451c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-23 09:49:50 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Exercise/student_academy.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/student_academy.py
@@ -5,3 +5,35 @@
 # Print the final dictionary with students and their average grade in the following format:
 # "{name} -> {averageGrade}"
 # Format the average grade to the 2nd decimal place.
+
+def get_average_grade(current):
+    for x in current:
+        result = sum(current) / len(current)
+        return result
+
+
+def only_above_average():
+    for current_student in grades:
+        average = 4.50
+        if get_average_grade(grades[current_student]) >= average:
+            student_average = get_average_grade(grades[current_student])
+            print(f"{current_student} -> {student_average:.2f}")
+
+
+grades = dict()
+
+students_number = int(input())
+
+for student in range(students_number):
+    name = input()
+    grade = float(input())
+    if name not in grades:
+        grades[name] = []
+        grades[name].append(grade)
+    else:
+        grades[name].append(grade)
+
+only_above_average()
+
+
+
