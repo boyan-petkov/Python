8cdc8f0f7dd3bd8868a398d5dcb715f61518f46f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-23 09:44:32 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Exercise/count_chars_in_a_string.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/count_chars_in_a_string.py
@@ -1,3 +1,17 @@
 # Write a program that counts all characters in a string except for space (" "). 
 # Print all the occurrences in the following format:
 # "{char} -> {occurrences}"
+
+sentence = input()
+
+dictionary = dict()
+
+for chr in sentence:
+    if chr not in dictionary and chr != " ":
+        dictionary[chr] = 1
+    else:
+        if chr != " ":
+            dictionary[chr] += 1
+
+for (key, value) in dictionary.items():
+    print(f"{key} -> {value}")
