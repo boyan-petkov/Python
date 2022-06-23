a7d39fd8628a38f5e5b9c1ed0424ec6181759c43
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-23 09:47:14 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Exercise/phonebook.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/phonebook.py
@@ -4,3 +4,18 @@
 # After filling the phonebook, you will receive a number – N.
 # Your program should be able to perform a search of contact by name and print its details in the format: "{name} -> {number}".
 #   In case the contact isn't found, print: "Contact {name} does not exist."
+
+info = input()
+contacts = dict()
+
+while not info.isdigit():
+    name, num = info.split("-")
+    contacts[name] = num
+    info = input()
+
+for x in range(int(info)):
+    current = input()
+    if current in contacts:
+        print(f"{current} -> {contacts[current]}")
+    else:
+        print(f"Contact {current} does not exist.")
