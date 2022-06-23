556be81c8f5a1e59949a201eeaa2916729ee70cc
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-23 09:46:48 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Exercise/capitals.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/capitals.py
@@ -1,3 +1,15 @@
 # Using a dictionary comprehension, write a program that receives country names on the first line, 
 # separated by comma and space ", ", and their corresponding capital cities on the second line (again separated by comma and space ", "). 
 # Print each country with their capital on a separate line in the following format: "{country} -> {capital}".
+
+def capital_final():
+    country = input().split(", ")
+    capitals = input().split(", ")
+
+    final = dict(zip(country, capitals))
+
+    for coun, cap in final.items():
+        print(f"{coun} -> {cap}")
+
+
+capital_final()
