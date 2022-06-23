7000ecc9e957ef9d177e400e7d65266a5694ac17
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-23 09:49:13 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - Exercise/courses.py
+++ Fundamentals/Lab and Exercises/Dictionaries - Exercise/courses.py
@@ -2,3 +2,22 @@
 # You will be receiving a course name and a student name until you receive the command "end". 
 # You should register each user into the corresponding course. If the given course does not exist, add it. 
 # When you receive the command "end", print the courses with their names and total registered users. For each course, print the registered users.
+
+
+registered_users = dict()
+
+cmd = input()
+
+while not cmd == "end":
+    course, student = cmd.split(" : ")
+    if course not in registered_users:
+        registered_users[course] = []
+        registered_users[course].append(student)
+    else:
+        registered_users[course].append(student)
+    cmd = input()
+
+for course in registered_users:
+    print(f"{course}: {len(registered_users[course])}")
+    for el in registered_users[course]:
+        print(f"-- {el}")
