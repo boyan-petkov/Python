28348f495da411b837a2b4138e82ae70b73dcae9
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-24 14:52:36 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Dictionaries - More Exercises/moba_challenger.py
+++ Fundamentals/Lab and Exercises/Dictionaries - More Exercises/moba_challenger.py
@@ -19,3 +19,53 @@
 # - {position1} <::> {skill}
 # - {position2} <::> {skill}
 
+
+def sum_skills(some_dict):
+    total = 0
+    for values in some_dict.values():
+        total += values
+    return total
+
+def compare(dict1, dict2):
+    for key in dict1:
+        if key in dict2:
+            return True
+
+players_pool = dict()
+
+command = input()
+while not command == "Season end":
+    if "->" in command:
+        player, position, skill = command.split(" -> ")
+        skill = int(skill)
+        if player not in players_pool:
+            players_pool[player] = {position: skill}
+        elif player in players_pool and position in players_pool[player]:
+            if players_pool[player][position] < skill:
+                players_pool[player][position] = skill
+        elif player in players_pool and position not in players_pool[player]:
+            players_pool[player][position] = skill
+    elif " vs " in command:
+        player1, player2 = command.split(" vs ")
+        if player1 in players_pool and player2 in players_pool:
+            if compare(players_pool[player1], players_pool[player2]):
+                player1_skills = sum_skills(players_pool[player1])
+                player2_skills = sum_skills((players_pool[player2]))
+                if player1_skills > player2_skills:
+                    del players_pool[player2]
+                elif player2_skills > player1_skills:
+                    del players_pool[player1]
+                else:
+                    continue
+    command = input()
+
+sorted_names = dict(sorted(players_pool.items(), key=lambda x: (-sum(x[1].values()), x[0])))
+
+for k, v in sorted_names.items():
+    current = k
+    print(f"{k}: {sum(v.values())} skill")
+    sorted_skills = (sorted(sorted_names[current].items(), key=lambda x: (-x[1], x[0])))
+    for item in sorted_skills:
+        print(f"- {item[0]} <::> {item[1]}")
+
+
