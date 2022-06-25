9a32b7e41dbd760f64d3613ab951497529c2a4c8
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-25 17:11:54 +0300

    Create digits_letters_and_others.py

---
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/digits_letters_and_others.py
@@ -0,0 +1,3 @@
+# Write a program which receives a single string. 
+# On the first line it should print all the digits found in the string, on the second – all the letters, and on the third – all the other characters. 
+# There will always be at least one digit, one letter and one other characters.
