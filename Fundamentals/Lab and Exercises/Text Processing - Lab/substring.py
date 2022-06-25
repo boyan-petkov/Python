b7b7ba75a853abc60213120ef9fd911dce05fcd7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-25 17:10:34 +0300

    Create substring.py

---
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/substring.py
@@ -0,0 +1,3 @@
+# On the first line you will receive a string. On the second line you will receive a second string. 
+# Write a program which removes all the occurrences of the first string in the second until there is no match. 
+# At the end print the remaining string.
