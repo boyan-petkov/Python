7cc7890ae2d3a8030d78a1b653a12307a00f5984
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-25 17:10:01 +0300

    Create repeat_strings.py

---
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/repeat_strings.py
@@ -0,0 +1,2 @@
+# Write a program which reads a sequence of strings, separated by a single space. 
+# Each string should be repeated N times, where N is the length of the string. Print the final strings concatenated into one string.
