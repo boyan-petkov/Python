84df7e042597c6fdc2d95395ddb07f6a21a1d5c6
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-25 17:09:20 +0300

    Create reverse_strings.py

---
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/reverse_strings.py
@@ -0,0 +1,2 @@
+# You will be given series of strings until you receive an "end" command. 
+# Write a program that reverses strings and prints each pair on separate line in the format "{word} = {reversed word}".
