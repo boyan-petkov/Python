140eac224670e134bd5c2aa43404e95e61a2a82e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-26 09:17:27 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Lab/repeat_strings.py
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/repeat_strings.py
@@ -1,2 +1,10 @@
 # Write a program which reads a sequence of strings, separated by a single space. 
 # Each string should be repeated N times, where N is the length of the string. Print the final strings concatenated into one string.
+
+words = input().split()
+
+word = ""
+
+for current_word in words:
+    word += current_word * len(current_word)
+print(word)
