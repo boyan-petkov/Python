36b818e1c16d5bd7c21c3d8d83259b9018b9dcf6
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-26 09:19:02 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Lab/digits_letters_and_others.py
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/digits_letters_and_others.py
@@ -1,3 +1,20 @@
 # Write a program which receives a single string. 
 # On the first line it should print all the digits found in the string, on the second – all the letters, and on the third – all the other characters. 
 # There will always be at least one digit, one letter and one other characters.
+
+letters = ""
+digits = ""
+symbols = ""
+
+text = input()
+
+for char in text:
+    if char.isalpha():
+        letters += char
+    elif char.isdigit():
+        digits += char
+    else:
+        symbols += char
+print(digits)
+print(letters)
+print(symbols)
