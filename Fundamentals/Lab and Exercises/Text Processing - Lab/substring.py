756cca656d30f25b48c7bc96344183f66a72dcb4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-26 09:18:02 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Lab/substring.py
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/substring.py
@@ -1,3 +1,12 @@
 # On the first line you will receive a string. On the second line you will receive a second string. 
 # Write a program which removes all the occurrences of the first string in the second until there is no match. 
 # At the end print the remaining string.
+
+searched = input()
+
+text = input()
+
+
+while searched in text:
+    text = text.replace(searched, "")
+print(text)
