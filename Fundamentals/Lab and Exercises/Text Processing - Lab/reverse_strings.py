8fa4126e579d2ee60c5e036fa58807549834be36
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-26 09:16:55 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Lab/reverse_strings.py
+++ Fundamentals/Lab and Exercises/Text Processing - Lab/reverse_strings.py
@@ -1,2 +1,14 @@
 # You will be given series of strings until you receive an "end" command. 
 # Write a program that reverses strings and prints each pair on separate line in the format "{word} = {reversed word}".
+
+
+word = input()
+
+while not word == "end":
+    word_reverse = reversed(word)
+    word_reverse = "".join(word_reverse)
+    print(f"{word} = {word_reverse}")
+
+
+
+    word = input()
