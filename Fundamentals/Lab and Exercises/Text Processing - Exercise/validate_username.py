e0870bf893ed02af6d9980a3c5ded9cef0bc36b3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-27 10:13:06 +0300

    Create validate_username.py

---
+++ Fundamentals/Lab and Exercises/Text Processing - Exercise/validate_username.py
@@ -0,0 +1,5 @@
+# Write a program that reads usernames on a single line (separated by ", ") and prints all valid usernames on separate lines. 
+# A valid username:
+# •	has length between 3 and 16 characters inclusive
+# •	can contain only letters, numbers, hyphens, and underscores
+# •	has no redundant symbols before, after, or in between
