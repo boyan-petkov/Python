1899d459053191388d453b6c3d4216a437a6ff0d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-28 15:50:39 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Exercise/extract_files.py
+++ Fundamentals/Lab and Exercises/Text Processing - Exercise/extract_files.py
@@ -1,1 +1,9 @@
 # Write a program that reads the path to a file and subtracts the file name and its extension.
+
+path = input()
+
+path = path.split("\\")
+
+name, extension = path[-1].split(".")
+print(f"File name: {name}")
+print(f"File extension: {extension}")
