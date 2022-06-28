53b77ddb5d76c880ef5bc9921289df0bedecccea
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-28 15:49:42 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Exercise/validate_username.py
+++ Fundamentals/Lab and Exercises/Text Processing - Exercise/validate_username.py
@@ -3,3 +3,35 @@
 # •	has length between 3 and 16 characters inclusive
 # •	can contain only letters, numbers, hyphens, and underscores
 # •	has no redundant symbols before, after, or in between
+
+def length(name):
+    if 3 <= len(name) <= 16:
+        return True
+    return False
+
+
+def allowed_chars(name):
+    for char in name:
+        if not (char.isalnum() or char == "-" or char == "_"):
+            return False
+    return True
+
+
+def redundant(name):
+    for char in name:
+        if char == " ":
+            return False
+    return True
+
+
+def is_valid(name):
+    if length(name) and allowed_chars(name) and redundant(name):
+        return True
+    return False
+
+
+usernames = input().split(", ")
+
+for current_name in usernames:
+    if is_valid(current_name):
+        print(current_name)
