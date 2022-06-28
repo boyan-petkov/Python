02bb49fa456203564e96476165d05fb989dd4d1f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-28 15:52:06 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Exercise/emoticon_finder.py
+++ Fundamentals/Lab and Exercises/Text Processing - Exercise/emoticon_finder.py
@@ -1,2 +1,10 @@
 # Find all emoticons in the text. An emoticon always starts with ":" and is followed by a symbol.
 # The input will be provided as a single string.
+
+string = input()
+
+for i in range(len(string)):
+    if string[i] == ":":
+        get = string[i + 1]
+
+        print(f"{':'}{get}")
