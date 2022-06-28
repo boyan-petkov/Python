171b125e3cc55080da4d4d09aaf65d03bd7b0021
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-28 15:51:23 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Exercise/ceaser_cipher.py
+++ Fundamentals/Lab and Exercises/Text Processing - Exercise/ceaser_cipher.py
@@ -1,3 +1,11 @@
 # Write a program that returns an encrypted version of the same text. 
 # Encrypt the text by replacing each character with the corresponding character three positions forward in the ASCII table. 
 # For example, A would be replaced with D, B would become E, and so on. Print the encrypted text.
+
+text = input()
+new_text = ""
+
+for char in text:
+    encrypted = ord(char) + 3
+    new_text += chr(encrypted)
+print(new_text)
