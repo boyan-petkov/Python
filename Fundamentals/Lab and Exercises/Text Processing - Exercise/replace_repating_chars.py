14bc25262975b61d195262770c61dfd12816ee7c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-28 15:52:38 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Exercise/replace_repating_chars.py
+++ Fundamentals/Lab and Exercises/Text Processing - Exercise/replace_repating_chars.py
@@ -1,1 +1,9 @@
 # Write a program that reads a string from the console and replaces any sequence of the same letters with a single corresponding letter.
+
+string = input()
+
+for i in range(len(string)):
+    if string[i] == ":":
+        get = string[i + 1]
+
+        print(f"{':'}{get}")
