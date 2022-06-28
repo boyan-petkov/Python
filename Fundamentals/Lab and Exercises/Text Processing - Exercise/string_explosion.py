3a96b182d480fca5cfa36995ed205d228b8068c2
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-28 15:53:05 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Text Processing - Exercise/string_explosion.py
+++ Fundamentals/Lab and Exercises/Text Processing - Exercise/string_explosion.py
@@ -5,3 +5,22 @@
 # Your task is to delete x characters, starting after the exploded character ('>'). If you find another explosion mark ('>') while deleting characters, 
 # you should add the strength to your previous explosion. You should not delete the explosion character – '>'.
 # When all characters are processed, print the final string. 
+
+string = input()
+final = ""
+strength = 0
+
+for i in range(len(string)):
+    temp = string[i]
+    if strength > 0 and string[i] != ">":
+        strength -= 1
+    elif string[i] == ">":
+        strength += int(string[i + 1])
+        final += string[i]
+    else:
+        final += string[i]
+
+
+
+print(final)
+          
