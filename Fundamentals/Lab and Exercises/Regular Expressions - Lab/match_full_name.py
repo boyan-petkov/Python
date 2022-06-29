905114c7d29ffc1d9caba351d38548bae827148e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-29 14:24:11 +0300

    Create match_full_name.py

---
+++ Fundamentals/Lab and Exercises/Regular Expressions - Lab/match_full_name.py
@@ -0,0 +1,9 @@
+# Write a program to match full names from a sequence of characters and print them on the console.
+# Writing the Regular Expression
+# First, write a regular expression to match a valid full name, according to these conditions:
+# •	A valid full name has the following characteristics:
+# o	It consists of two words.
+# o	Each word starts with a capital letter.
+# o	After the first letter, it only contains lowercase letters.
+# o	Each of the two words should be at least two letters long.
+# o	A single space separates the two words.
