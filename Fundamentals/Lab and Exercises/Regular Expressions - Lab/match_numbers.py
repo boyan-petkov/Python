f78406ec1adfdccb5a4877278916e100ce732e68
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-06-30 11:15:54 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Regular Expressions - Lab/match_numbers.py
+++ Fundamentals/Lab and Exercises/Regular Expressions - Lab/match_numbers.py
@@ -9,3 +9,22 @@
 # •	The decimal part (if it exists) consists of a period (".") and one or more digits after it. Use a capturing group.
 # •	Has either whitespace before it or the end of the string (match either $ or what's called a positive lookahead). 
 #   The syntax for the end of the RegEx might look something like "($|(?=\s))".
+
+import re
+
+
+numbers = input()
+
+expression = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+
+matches = re.finditer(expression, numbers)
+
+# for element in matches:
+#     result = element.group()
+#     print(result, end=" ")
+
+final = list()    
+for match in matches:
+    final.append(match.group())
+
+print(" ".join(final))
