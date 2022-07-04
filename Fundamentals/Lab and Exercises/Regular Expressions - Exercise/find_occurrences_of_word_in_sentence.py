fd7ee5d3f0f1c5fcb5c3d2b6795ed55feb13c090
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-04 12:59:43 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Regular Expressions - Exercise/find_occurrences_of_word_in_sentence.py
+++ Fundamentals/Lab and Exercises/Regular Expressions - Exercise/find_occurrences_of_word_in_sentence.py
@@ -1,3 +1,20 @@
 # Write a program that finds how many times a word is used in a string. 
 # The output is a single number indicating the number of times the string contains the word.
 # Note that letter case does not matter – it is case-insensitive.
+
+# Input:
+# How do you plan on achieving that? How? How can you even think of that? how
+# Output:
+# 3
+
+import re
+
+string = input()
+
+searched = input()
+
+expression = fr"\b{searched}\b"
+
+matches = re.findall(expression, string, flags=re.I)
+
+print(len(matches))
