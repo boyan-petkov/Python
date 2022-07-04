699e38cc5386208314c652e2555b1b173176d9eb
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-04 12:57:49 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Regular Expressions - Exercise/find_variable_names_in_sentences.py
+++ Fundamentals/Lab and Exercises/Regular Expressions - Exercise/find_variable_names_in_sentences.py
@@ -2,3 +2,21 @@
 # A variable name starts with an underscore ("_") and contains only capital and non-capital letters and digits.
 # Extract only their names without the underscore. Try to do this only with regular expressions.
 # The output consists of all variable names extracted and printed on a single line, separated by a comma.
+
+
+# Input:
+# __invalidVariable _evenMoreInvalidVariable_ _validVariable
+# Output:
+# validVariable
+
+import  re
+
+text = input()
+
+expression = r"\b_([A-Za-z0-9]+)\b"
+
+matches = re.findall(expression, text)
+
+final = list()
+
+print(",".join(matches))
