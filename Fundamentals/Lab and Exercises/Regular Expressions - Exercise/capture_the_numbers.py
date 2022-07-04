e7a57ef0f679c1ebb22d010954e7feda1bbcefdb
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-04 12:56:09 +0300

    Initial commit

--- Fundamentals/Lab and Exercises/Regular Expressions - Exercise/capture_the_numbers.py
+++ Fundamentals/Lab and Exercises/Regular Expressions - Exercise/capture_the_numbers.py
@@ -1,2 +1,33 @@
 # Write a program that receives strings on different lines and extracts only the numbers. 
 # Print all extracted numbers on a single line, separated by a single space.
+
+# Input: 
+#   The300
+#   What is that?
+#   I think it's the 3rd movie 
+#   Let's watch it at 22:45
+
+# Output:
+#   300 3 22 45
+
+
+import re
+
+lines = []
+while True:
+    line = input()
+    if line:
+        lines.append(line)
+    else:
+        break
+text = ' '.join(lines)
+
+expression = r"\d+"
+
+searched = re.findall(expression, text)
+
+final = list()
+
+for el in searched:
+    final.append(el)
+print(" ".join(final))
