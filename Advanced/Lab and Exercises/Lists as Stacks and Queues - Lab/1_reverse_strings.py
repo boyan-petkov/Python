9d056c3866e5bc0464e499202713e4c6912ad386
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-08 11:32:43 +0300

    Initial commit

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Lab/1_reverse_strings.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Lab/1_reverse_strings.py
@@ -2,3 +2,15 @@
 # •	Reads an input string
 # •	Reverses it using a stack
 # •	Prints the result back on the console
+
+def add_it(text):
+    while len(text) > 0:
+        stacks.append(string.pop())
+    return "".join(stacks)
+
+
+string = list(input())
+
+stacks = list()
+
+print(add_it(string))
