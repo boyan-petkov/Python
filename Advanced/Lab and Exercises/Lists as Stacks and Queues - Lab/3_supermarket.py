d52ee046704fba309a4dce1d0760fdcc7329d0a8
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-08 11:33:54 +0300

    Initial commit

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Lab/3_supermarket.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Lab/3_supermarket.py
@@ -2,3 +2,17 @@
 # Write a program which reads lines of input consisting of a customer's name and adds it to the end of a queue until "End" is received. 
 # If in a meantime you receive the command "Paid", you should print each customer in the order they are served (from the first to the last one) and empty the queue. 
 # When you receive "End", you should print the count of the remaining people in the queue in the format: "{count} people remaining.".
+
+from collections import deque
+
+clients = deque()
+
+command = input()
+while not command == "End":
+    if command == "Paid":
+        for i in range(len(clients)):
+            print(clients.popleft())
+    else:
+        clients.append(command)
+    command = input()
+print(f"{len(clients)} people remaining.")
