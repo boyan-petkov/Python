40df821458e1794b85dda9eb76ac599ece1540e4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-08 11:33:22 +0300

    Initial commit

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Lab/2_matching_parentheses.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Lab/2_matching_parentheses.py
@@ -1,2 +1,19 @@
 # You are given an algebraic expression with parentheses. Scan through the string and extract each set of parentheses.
 # Print the result back on the console.
+
+def get_them(text):
+    for i in range(len(text)):
+        current = text[i]
+        if text[i] == "(":
+            stack.append(i)
+        elif text[i] == ")":
+            start = stack.pop()
+            end = i
+            print("".join(text[start:end + 1]))
+
+
+some_string = list(input())
+
+stack = list()
+get_them(some_string)
+
