9f07c353dde828e4c14bcc307056e080514b2d08
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-09 13:54:35 +0300

    Update and rename fast_food.py to 3_fast_food.py

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/3_fast_food.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/3_fast_food.py
@@ -7,13 +7,16 @@ Keep the orders in a queue.
 Find the biggest order and print it. Next, you will begin servicing your clients from the first one that came. 
 Before each order, check if you have enough food left to complete it. 
 If you have, remove the order from the queue and reduce the amount of food you have. Otherwise, stop serving.
+
 Input
 •	On the first line you will be given the quantity of your food - an integer in the range [0, 1000]
 •	On the second line you will receive a sequence of integers, representing each order, separated by a single space
+
 Output
 •	On the first line, print the quantity of biggest order
 •	On the second line:
 o	If you succeeded in servicing all your clients, print: "Orders complete".  
 o	Otherwise, print: "Orders left: {order1} {order2} .... {orderN}".
+  
 Constraints
 •	The input will always be valid
