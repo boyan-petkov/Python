6cc2280f4a34264f1e58770365b54545c4838ed5
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-09 13:55:34 +0300

    Update and rename truck_tour.py to 5_truck_tour.py

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/5_truck_tour.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/5_truck_tour.py
@@ -5,12 +5,15 @@ For each petrol pump you will receive two pieces of information (separated by a 
 Initially, you have a tank of infinite capacity carrying no petrol. 
 The truck can start the tour at any of the petrol pumps and it will move one kilometer for each liter of the petrol.
 Calculate the first point from where the truck will be able to complete the circle. Consider that the truck will stop at each of the petrol pumps. 
+
 Input
 •	On the first line you will receive the number of petrol pumps - N
 •	On the next N-lines you will receive the amount of petrol that petrol pump will give and 
 the distance between that petrol pump and the next petrol pump, separated by single space
+
 Output
 •	An integer which will be the smallest index of a petrol pump from which you can start the tour
+
 Constraints
 •	1 ≤ N ≤ 1000001
 •	1 ≤ Amount of petrol, Distance ≤ 1000000000
