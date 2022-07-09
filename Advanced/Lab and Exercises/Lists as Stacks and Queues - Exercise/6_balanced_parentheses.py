f730d592452f5bb34364d7a3b89448003a805fdd
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-09 13:56:08 +0300

    Update and rename balanced_parentheses.py to 6_balanced_parentheses.py

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/6_balanced_parentheses.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/6_balanced_parentheses.py
@@ -4,11 +4,14 @@ There will be no interval symbols between the parentheses. You will be given thr
 {[()]} - Parentheses are balanced.
 (){}[] - Parentheses are balanced.
 {[(])} - Parentheses are NOT balanced.
+  
 Input
 •	On a single line you will receive a sequence of parentheses.
+  
 Output 
 •	For each test case, print on a new line "YES" if the parentheses are balanced. 
 Otherwise, print "NO". Do not print the quotes.
+  
 Constraints
 •	1 ≤ lens ≤ 1000, where lens is the length of the sequence.
 •	Each character of the sequence will be one of {, }, (, ), [, ].
