3b6f563adf35e06e061b905f206738aa4ada5765
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-09 13:45:47 +0300

    Create reverse.numbers.py

---
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/reverse.numbers.py
@@ -0,0 +1,2 @@
+Write a program which reads from the console a string with N integers, separated by a single space, and reverses them using a stack. 
+Print the reversed integers on one line, separated by a single space.
