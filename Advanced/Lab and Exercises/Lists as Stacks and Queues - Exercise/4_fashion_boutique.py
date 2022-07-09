faacb76ad44fd2e94c8a2833af3bf05d5e9eeb20
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-09 13:55:04 +0300

    Update and rename fashion_boutique.py to 4_fashion_boutique.py

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/4_fashion_boutique.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/4_fashion_boutique.py
@@ -6,11 +6,14 @@ Use a stack for the purpose. Each piece of clothing has its value (an integer). 
 •	If the sum becomes equal to the capacity of the current rack you must take a new one for the next clothes (if there are any left in the box). 
 •	If the sum becomes greater than the capacity, do not hang the piece of clothing to the current rack. Take a new rack and then hang it up.
 In the end, print how many racks you have used to hang up the clothes.
+
 Input
 •	On the first line you will be given a sequence of integers, representing the clothes in the box, separated by a single space.
 •	On the second line, you will be given an integer, representing the capacity of a rack.
+
 Output
 •	Print the number of racks, needed to hang up the clothes from the box.
+
 Constraints
 •	The values of the clothes will be integers in the range [0,20]
 •	There will never be more than 50 clothes in a box
