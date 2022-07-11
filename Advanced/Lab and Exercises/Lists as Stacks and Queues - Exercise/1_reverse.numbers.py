5814ed8fc6c5c4daa0421475d74b1e8ec8441a31
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-11 10:15:59 +0300

    Initial commit

--- Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/1_reverse.numbers.py
+++ Advanced/Lab and Exercises/Lists as Stacks and Queues - Exercise/1_reverse.numbers.py
@@ -1,2 +1,9 @@
-Write a program which reads from the console a string with N integers, separated by a single space, and reverses them using a stack. 
-Print the reversed integers on one line, separated by a single space.
+# Write a program which reads from the console a string with N integers, separated by a single space, and reverses them using a stack. 
+# Print the reversed integers on one line, separated by a single space.
+
+from collections import deque
+
+nums = deque(input().split())
+
+while nums:
+    print(nums.pop(), end=" ")
