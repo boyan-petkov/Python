0cafeb467f9d87e51832e0333425119b1eb7ad66
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-12 14:46:21 +0300

    Create count_same_values.py

---
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/count_same_values.py
@@ -0,0 +1,3 @@
+You will be given numbers separated by a space.
+Write a program that prints the number of occurrences of each number in the format "{number} - {count} times". 
+The number must be formatted to the first decimal point.
