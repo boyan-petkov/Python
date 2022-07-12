df34f46b64a5fb00635377a5d408e0822817d96a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-12 14:47:33 +0300

    Create students_grades.py

---
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/students_grades.py
@@ -0,0 +1,5 @@
+Write a program that reads students' names and their grades and adds them to the student record.
+On the first line, you will receive the number of students – N. On the following N lines, you will be receiving a student's name and their grade.
+For each student print all his/her grades and finally his/her average grade, formatted to the second decimal point in the format: 
+  "{student's name} -> {grade1} {grade2} ... {gradeN} (avg: {average_grade})".
+The order in which we print the result does not matter.
