93836acd9c4f4daeccebd790f29f8ffb1d2d7c5c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-12 14:50:16 +0300

    Update and rename record_unique_names.py to 3_record_unique_names.py

---
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/3_record_unique_names.py
@@ -0,0 +1,2 @@
+# Write a program, which will take a list of names and print only the unique names in the list.
+# The order in which we print the result does not matter.
--- Advanced/Lab and Exercises/Tuples and Sets - Lab/record_unique_names.py
+++
@@ -1,2 +0,0 @@
-Write a program, which will take a list of names and print only the unique names in the list.
-The order in which we print the result does not matter.
