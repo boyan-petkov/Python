cbd65719d810239cc5d7c436c00363063d416127
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-13 09:29:54 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Lab/1_count_same_values.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/1_count_same_values.py
@@ -1,3 +1,15 @@
 # You will be given numbers separated by a space.
 # Write a program that prints the number of occurrences of each number in the format "{number} - {count} times". 
 # The number must be formatted to the first decimal point.
+
+
+nums = [float(x) for x in input().split()]
+
+final = dict()
+for num in nums:
+    if num not in final:
+        final[num] = 0
+    final[num] += 1
+
+for kvp in final.items():
+    print(f"{kvp[0]} - {kvp[1]} times")
