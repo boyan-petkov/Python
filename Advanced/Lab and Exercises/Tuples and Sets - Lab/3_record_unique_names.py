2c880662637795293fbe4c5b1c8b31613332af02
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-13 09:30:54 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Lab/3_record_unique_names.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/3_record_unique_names.py
@@ -1,2 +1,11 @@
 # Write a program, which will take a list of names and print only the unique names in the list.
 # The order in which we print the result does not matter.
+
+names_num = int(input())
+
+unique = set()
+for _ in range(names_num):
+    name = input()
+    unique.add(name)
+
+print('\n'.join(unique))
