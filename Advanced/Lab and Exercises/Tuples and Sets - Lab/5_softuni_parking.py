7cf51522d788b54228089ce8f8c773ceebd8f4d5
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-13 09:31:36 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Lab/5_softuni_parking.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/5_softuni_parking.py
@@ -6,3 +6,45 @@
 # In the end, print the number of guests who did not come to the party and their reservation numbers:
 # •	The VIP guests must be first.
 # •	Both the VIP and the Regular guests must be sorted in ascending order
+
+def vip_or_not(current_code):
+    if current_code[0].isdigit():
+        vips.add(current_code)
+    elif current_code[0].isalpha():
+        regular.add(current_code)
+    return vips, regular
+
+
+def if_they_come(current_code):
+    if current_code in vips:
+        vips.remove(current_code)
+    if current_code in regular:
+        regular.remove(current_code)
+    return vips, regular
+
+
+#
+def union_and_print(sets1, sets2):
+    final = sets1.union(sets2)
+    # final = list(final)
+    # final.sort()
+    print(len(final))
+    print("\n".join(sorted(final)))
+
+
+invites_number = int(input())
+
+vips = set()
+regular = set()
+
+for _ in range(invites_number):
+    vip_or_not(input())
+
+cmd = input()
+while not cmd == "END":
+    if_they_come(cmd)
+    cmd = input()
+
+union_and_print(vips, regular)
+
+
