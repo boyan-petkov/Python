9c89acc6e43e3ae912838f7c36c07d9b75e90513
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-13 09:31:15 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Lab/4_parking_lot.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/4_parking_lot.py
@@ -6,3 +6,28 @@
 # The direction could only be "IN" or "OUT". Print the car numbers which are still in the parking lot. 
 # Keep in mind that all car numbers must be unique. If the parking lot is empty, print "Parking Lot is Empty".
 # Note: The order in which we print the result does not matter.
+
+def parking_lot(current_car):
+    direction, number = current_car.split(", ")
+    if direction == "IN":
+        lot.add(number)
+    elif direction == "OUT":
+        lot.remove(number)
+    return lot
+
+
+def print_them(some_collection):
+    if not some_collection:
+        print("Parking Lot is Empty")
+    else:
+        print("\n".join(some_collection))
+
+
+number_of_commands = int(input())
+
+lot = set()
+
+for _ in range(number_of_commands):
+    parking_lot(input())
+
+print_them(lot)
