ac51f6a60a99464abe87de5ee96f3d628b22fb2e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-13 09:30:18 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Lab/2_students_grades.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Lab/2_students_grades.py
@@ -3,3 +3,19 @@
 # For each student print all his/her grades and finally his/her average grade, formatted to the second decimal point in the format: 
 #   "{student's name} -> {grade1} {grade2} ... {gradeN} (avg: {average_grade})".
 # The order in which we print the result does not matter.
+
+students_grades = int(input())
+
+students = dict()
+
+for _ in range(students_grades):
+    name, grade_str = input().split()
+    grade = float(grade_str)
+    if name not in students:
+        students[name] = []
+    students[name].append(grade)
+
+for current_student in students.items():
+    student, grades = current_student[0], current_student[1]
+    print(f"{student} ->",' '.join([f"{x:.2f}" for x in grades]), f"(avg: {sum(grades) / len(grades):.2f})")
+
