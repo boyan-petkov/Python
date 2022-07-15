efd3be2fd5b83af4a21962d85bb42d926926f8ab
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-15 14:25:00 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Exercise/4_count_symbols.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Exercise/4_count_symbols.py
@@ -1,2 +1,16 @@
 # Write a program that reads a text from the console and counts the occurrences of each character in it. 
 # Print the results in alphabetical (lexicographical) order.  
+
+
+text = input()
+
+symbols = dict()
+
+for char in text:
+    if char not in symbols:
+        symbols[char] = 0
+    symbols[char] += 1
+
+for kvp in sorted(symbols.items()):
+    char, times = kvp[0], kvp[1]
+    print(f"{char}: {times} time/s")
