2e901c301d377f4c4a16eed537a25cd3fcc6a706
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-15 14:23:57 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Exercise/1_unique_usernames.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Exercise/1_unique_usernames.py
@@ -1,3 +1,13 @@
 # Write a program that reads from the console a sequence of N usernames and keeps a collection only of the unique ones. 
 # On the first line, you will receive an integer N. On the next N lines, you will receive a username. 
 # Print the collection on the console (the order does not matter):
+
+usernames_number = int(input())
+
+unique = set()
+
+for _ in range(usernames_number):
+    name = input()
+    unique.add(name)
+
+print("\n".join(unique))
