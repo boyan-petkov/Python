2ed51057a8a095cfe3bb26249ff7005fe1a55849
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-15 14:24:40 +0300

    Intiial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Exercise/3_periodic_table.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Exercise/3_periodic_table.py
@@ -2,3 +2,12 @@
 # On the first line you will be given a number n - the count of input lines that you are going to receive. 
 # On the next n lines, you will be receiving chemical compounds, separated by a single space. 
 # Your task is to print all the unique ones on separate lines (the order does not matter):
+
+
+number_of_inputs = int(input())
+some_set = set()
+
+for _ in range(number_of_inputs):
+    current = input().split()
+    some_set = some_set.union(current)
+print("\n".join(some_set)) 
