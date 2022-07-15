0d63b9266e3c332c31ce25e1e21912920cb2c394
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-15 14:25:49 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Exercise/6_battle_of_names.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Exercise/6_battle_of_names.py
@@ -5,3 +5,33 @@
 # •	If the sum of the odd numbers is bigger than the sum of the even numbers, print the different values, separated by ", ".
 # •	If the sum of the even numbers is bigger than the sum of the odd numbers, print the symmetric different values, separated by ", ".
 # NOTE: On every operation, the starting set should be the odd set
+
+
+lines_num = int(input())
+
+odd_set = set()
+even_set = set()
+
+even_sum = 0
+odd_sum = 0
+
+for current_row in range(1, lines_num + 1):
+    name = [ord(x) for x in input()]
+    name_value = sum(name) // current_row
+    if name_value % 2 == 0:
+        even_set.add(name_value)
+        even_sum += name_value
+    else:
+        odd_set.add(name_value)
+        odd_sum += name_value
+result = set()
+if even_sum == odd_sum:
+    result = even_set.union(odd_set)
+elif odd_sum > even_sum:
+    result = odd_set.difference(even_set)
+elif even_sum > odd_sum:
+    result = even_set.symmetric_difference(odd_set)
+print(", ".join([str(x) for x in result ]))
+
+
+
