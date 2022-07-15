b6e7c8eb3411b6df6dcc8f4bc98cd420ed29273a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-15 14:25:24 +0300

    Initial commit

--- Advanced/Lab and Exercises/Tuples and Sets - Exercise/5_longest_intersection.py
+++ Advanced/Lab and Exercises/Tuples and Sets - Exercise/5_longest_intersection.py
@@ -4,3 +4,42 @@
 # Finally, you should find the longest intersection of all N intersections, print the numbers that are included and its length in the format: 
 #   "Longest intersection is [{longest_intersection_numbers}] with length {length_longest_intersection}"
 # Note: in each range, there will always be intersection. If there are two equal intersections, print the first one.
+
+
+def divide_them(pair1, pair2):
+    start, end = [int(x)for x in pair1.split(",")]
+    start_, end_, = [int(x)for x in pair2.split(",")]
+    pair1 = set()
+    pair2 = set()
+    for num in range(start, end + 1):
+        nums_pair1.add(num)
+    for num in range(start_, end_ + 1):
+        nums_pair2.add(num)
+    return nums_pair1, nums_pair2
+
+
+def is_it_larger(first, second, largest, final):
+    global intersection
+    global current_largest
+    current_intersection = first.intersection(second)
+    if len(current_intersection) > largest:
+        largest = len(current_intersection)
+        final = current_intersection
+        intersection = final
+        current_largest = largest
+    return intersection, current_largest
+
+
+nums_pair1 = set()
+nums_pair2 = set()
+intersection = []
+current_largest = 0
+n_lines = int(input())
+
+for _ in range(n_lines):
+    line = input().split("-")
+    divide_them(line[0], line[1])
+    is_it_larger(nums_pair1, nums_pair2, current_largest, intersection)
+    nums_pair1.clear(), nums_pair2.clear()
+
+print(f"Longest intersection is [{', '.join([str(x)for x in intersection])}] with length {len(intersection)}")
