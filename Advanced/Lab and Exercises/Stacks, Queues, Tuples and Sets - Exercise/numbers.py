be4f9201c653f8d30020554741d30abbb309be8c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-16 10:19:01 +0300

    Create numbers.py

---
+++ Advanced/Lab and Exercises/Stacks, Queues, Tuples and Sets - Exercise/numbers.py
@@ -0,0 +1,10 @@
+First, you will be given two sequences of integers values on different lines. T
+he values of the sequences are separated by a single space between them.
+Keep in mind that each sequence should contain only unique values.
+Next, you will receive a number - N. On the next N lines, you will receive one of the following commands:
+•	"Add First {numbers, separated by a space}" - add the given numbers at the end of the first sequence of numbers.
+•	"Add Second {numbers, separated by a space}" - add the given numbers at the end of the second sequence of numbers.
+•	"Remove First {numbers, separated by a space}" - remove only the numbers contained in the first sequence.
+•	"Remove Second {numbers, separated by a space}" - remove only the numbers contained in the second sequence.
+•	"Check Subset" - check if any of the given sequences are a subset of the other. If it is, print "True". Otherwise, print "False".
+In the end, print the final sequences, separated by a comma and a space ", ". The values in each sequence should be sorted in ascending order.
