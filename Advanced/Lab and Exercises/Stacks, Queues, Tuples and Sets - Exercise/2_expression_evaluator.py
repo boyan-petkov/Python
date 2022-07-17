9e5efd4c360b9361a5fe95f997a4c2a1030d31f0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-17 08:19:00 +0300

    Initial commit

--- Advanced/Lab and Exercises/Stacks, Queues, Tuples and Sets - Exercise/2_expression_evaluator.py
+++ Advanced/Lab and Exercises/Stacks, Queues, Tuples and Sets - Exercise/2_expression_evaluator.py
@@ -21,3 +21,29 @@
 # •	Operators and numbers will always be valid
 # •	There will be no case of division by zero
 # •	There might be negative numbers in the string
+
+from collections import deque
+
+elements = input().split()
+
+numbers = deque()
+
+for el in elements:
+    if el in "-+/*":
+        while len(numbers) > 1:
+            first = numbers.popleft()
+            second = numbers.popleft()
+            result = 0
+            if el == "+":
+                result = first + second
+            elif el == "*":
+                result = first * second
+            elif el == "-":
+                result = first - second
+            elif el == "/":
+                result = first // second
+
+            numbers.appendleft(result)
+    else:
+        numbers.append(int(el))
+print(numbers.popleft())
