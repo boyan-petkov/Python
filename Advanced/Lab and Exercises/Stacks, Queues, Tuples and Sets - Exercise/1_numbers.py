d2701b74c8da754fadd0a2e32b07950fce58e938
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-17 08:18:28 +0300

    Initial commit

--- Advanced/Lab and Exercises/Stacks, Queues, Tuples and Sets - Exercise/1_numbers.py
+++ Advanced/Lab and Exercises/Stacks, Queues, Tuples and Sets - Exercise/1_numbers.py
@@ -8,3 +8,46 @@
 # •	"Remove Second {numbers, separated by a space}" - remove only the numbers contained in the second sequence.
 # •	"Check Subset" - check if any of the given sequences are a subset of the other. If it is, print "True". Otherwise, print "False".
 # In the end, print the final sequences, separated by a comma and a space ", ". The values in each sequence should be sorted in ascending order.
+
+def cycle_add(iterable, command):
+    for i in range(2, (len(command))):
+        iterable.add(command[i])
+    return iterable
+
+
+def remove_cycle(iterable, command):
+    for i in range(2, len(command)):
+        iterable.discard(command[i])
+    return iterable
+
+
+def subset(iterable1, iterable2):
+    if iterable1.issubset(iterable2) or iterable2.issubset(iterable1):
+        print("True")
+    else:
+        print("False")
+
+
+first_set = set(input().split())
+second_set = set(input().split())
+
+lines_number = int(input())
+
+for _ in range(lines_number):
+    cmd = input().split()
+    current_cmd = cmd[0] + " " + cmd[1]
+    if current_cmd == "Add First":
+        first_set = cycle_add(first_set, cmd)
+    elif current_cmd == "Add Second":
+        second_set = cycle_add(second_set, cmd)
+    elif current_cmd == "Remove First":
+        remove_cycle(first_set, cmd)
+    elif current_cmd == "Remove Second":
+        remove_cycle(second_set, cmd)
+    elif current_cmd == "Check Subset":
+        subset(first_set, second_set)
+
+first_set = [int(x) for x in first_set]
+second_set = [int(x) for x in second_set]
+print(*sorted(first_set), sep=", ")
+print(*sorted(second_set), sep=", ")
