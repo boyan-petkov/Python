bdaaad5221b5831a2d2572299f87197d37ff12d9
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-17 08:21:09 +0300

    Initial commit

--- Advanced/Lab and Exercises/Stacks, Queues, Tuples and Sets - Exercise/5_paint_colors.py
+++ Advanced/Lab and Exercises/Stacks, Queues, Tuples and Sets - Exercise/5_paint_colors.py
@@ -29,3 +29,53 @@
 # •	You will not receive an empty string
 # •	Please consider only the colors mentioned above
 # •	There won't be any cases with repeating colors
+
+from collections import deque
+
+materials = [int(x) for x in input().split()]
+magic_levels = deque([int(x) for x in input().split()])
+
+presents = {
+    150: ["Doll", 0],
+    250: ["Wooden train", 0],
+    300: ["Teddy bear", 0],
+    400: ["Bicycle", 0],
+}
+crafted = set()
+while materials and magic_levels:
+    material = materials.pop()
+    magic = magic_levels.popleft()
+    current = material * magic
+    if current in presents:
+        presents[current][1] += 1
+        crafted.add(presents[current][0])
+
+    else:
+        if current < 0:
+            current = material + magic
+            materials.append(current)
+        elif current > 0:
+            current = material + 15
+            materials.append(current)
+        elif material == 0 or magic == 0:
+            if material == 0 and magic == 0:
+                continue
+            if material == 0:
+                magic_levels.appendleft(magic)
+            elif magic == 0:
+                materials.append(material)
+
+if ("Doll" in crafted and "Wooden train" in crafted) or ("Teddy bear" in crafted and "Bicycle" in crafted):
+    print("The presents are crafted! Merry Christmas!")
+else:
+    print("No presents this Christmas!")
+if materials:
+    print(f"Materials left:", ', '.join([str(x) for x in reversed(materials)]))
+if magic_levels:
+    print(f"Magic left:", ', '.join([str(x) for x in (magic_levels)]))
+
+for value in sorted(presents.values()):
+    name, n = value[0], value[1]
+    if n > 0:
+
+        print(f"{name}: {n}")
