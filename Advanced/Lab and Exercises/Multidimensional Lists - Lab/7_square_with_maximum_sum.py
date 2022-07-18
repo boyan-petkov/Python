5989fdb2725843367eb6dc39b8a0178bbb6d97d2
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-18 10:05:14 +0300

    Update and rename square_with_maximum_sum.py to 7_square_with_maximum_sum.py

---
+++ Advanced/Lab and Exercises/Multidimensional Lists - Lab/7_square_with_maximum_sum.py
@@ -0,0 +1,4 @@
+# Write a program that reads a matrix from the console and finds the 2x2 top-left submatrix with biggest sum of its values. 
+# On first line you will get matrix sizes in format "{rows}, {columns}". 
+# On the next rows, you will get elements for each column, separated with a comma and a space ", ". 
+# You should print the found submatrix and the sum of its elements as shown in the examples. 
