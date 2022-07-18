98e356e0bd42d5ec7bf808a046b08080d28e4bb7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-18 09:56:28 +0300

    Update and rename primary_diagonal.py to 4_primary_diagonal.py

---
+++ Advanced/Lab and Exercises/Multidimensional Lists - Lab/4_primary_diagonal.py
@@ -0,0 +1,3 @@
+# Write a program that finds the sum of all numbers in a matrix's primary diagonal (runs from top left to bottom right). 
+# On the first line, you will receive an integer N – the size of a square matrix. 
+# The next N lines holds the values for each column - N numbers, separated by a single space. 
