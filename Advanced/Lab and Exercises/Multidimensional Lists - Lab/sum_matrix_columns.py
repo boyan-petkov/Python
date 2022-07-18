af6a1f85a3b40a92ce73e2bf5dadf44c811ede4a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-18 09:52:51 +0300

    Create sum_matrix_columns.py

---
+++ Advanced/Lab and Exercises/Multidimensional Lists - Lab/sum_matrix_columns.py
@@ -0,0 +1,3 @@
+Write a program that reads a matrix from the console and prints the sum for each column on separate lines. 
+On the first line, you will get matrix sizes in format "{rows}, {columns}". 
+On the next rows, you will get elements for each column separated with a single space. 
