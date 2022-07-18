9bdb56b0f0d982900155308305b8bc30a5121ff0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-18 09:55:45 +0300

    Update and rename even_matrix.py to 2_even_matrix.py

---
+++ Advanced/Lab and Exercises/Multidimensional Lists - Lab/2_even_matrix.py
@@ -0,0 +1,3 @@
+# Write a program that receives a matrix of numbers and prints a new one only with the numbers that are even. 
+# Use nested comprehension for that problem. 
+# On the first line, you will receive the rows of the matrix. On the next rows, you will get elements for each column separated with a comma and a space ", ".  
