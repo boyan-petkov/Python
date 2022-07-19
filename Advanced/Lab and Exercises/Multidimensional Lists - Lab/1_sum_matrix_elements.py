77e3d75d0c4bc70ae493a19459f382fd8ffc6957
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-19 09:06:21 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Lab/1_sum_matrix_elements.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Lab/1_sum_matrix_elements.py
@@ -3,3 +3,15 @@
 # •	The matrix itself
 # On the first line, you will receive the matrix sizes in the format "{rows}, {columns}". 
 # On the next rows, you will get elements for each column separated by a comma and a space ", ". 
+
+rows, columns = [int(el) for el in input().split(", ")]
+
+matrix = []
+total = 0
+for row in range(rows):
+    nums = [int(el) for el in input().split(", ")]
+    total += sum(nums)
+    matrix.append(nums)
+print(total)
+print(matrix)
+    
