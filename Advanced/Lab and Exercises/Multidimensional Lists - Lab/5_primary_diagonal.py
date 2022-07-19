c714890a5e8c00bfc9cba4fff6ee8ae9b99374c9
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-19 09:08:10 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Lab/5_primary_diagonal.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Lab/5_primary_diagonal.py
@@ -1,3 +1,21 @@
 # Write a program that finds the sum of all numbers in a matrix's primary diagonal (runs from top left to bottom right). 
 # On the first line, you will receive an integer N – the size of a square matrix. 
 # The next N lines holds the values for each column - N numbers, separated by a single space. 
+
+rows = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    nums = [int(el) for el in input().split()]
+    matrix.append(nums)
+
+diagonal = 0
+
+for row_i in range(rows):
+    for col_i in range(len(matrix[row_i])):
+        if row_i == col_i:
+            diagonal += matrix[row_i][col_i]
+
+
+print(diagonal)
