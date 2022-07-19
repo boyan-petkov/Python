573fce87b6c6b36f52befb32f73af2b9528ff97a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-19 09:06:45 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Lab/2_even_matrix.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Lab/2_even_matrix.py
@@ -1,3 +1,12 @@
 # Write a program that receives a matrix of numbers and prints a new one only with the numbers that are even. 
 # Use nested comprehension for that problem. 
 # On the first line, you will receive the rows of the matrix. On the next rows, you will get elements for each column separated with a comma and a space ", ".  
+
+rows = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    nums = [int(el) for el in input().split(", ") if int(el) % 2 == 0]
+    matrix.append(nums)
+print(matrix)
