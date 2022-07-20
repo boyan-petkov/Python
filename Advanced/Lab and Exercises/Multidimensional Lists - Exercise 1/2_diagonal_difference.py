4286eadcb530c170aef913822478de94d55b1d80
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-20 16:35:47 +0300

    Update and rename diagonal_difference.py to 2_diagonal_difference.py

---
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/2_diagonal_difference.py
@@ -0,0 +1,4 @@
+# Write a program that finds the difference between the sums of the square matrix diagonals (absolute value).
+# On the first line, you will receive an integer N - the size of a square matrix. 
+# The following N lines hold the values for each column - N numbers separated by a single space. 
+# Print the absolute difference between the primary and the secondary diagonal sums.
