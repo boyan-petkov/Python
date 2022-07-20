ac3347143cba47a1c593142fe0e856ab6c2e9b10
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-20 16:29:59 +0300

    Create maximal_sum.py

---
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/maximal_sum.py
@@ -0,0 +1,10 @@
+Write a program that reads a rectangular matrix's dimensions and finds the 3x3 square with a maximum sum of its elements. 
+There will be no case with two or more 3x3 squares with equal maximal sum.
+
+Input
+•	On the first line, you will receive the rows and columns in the format "{rows} {columns}" – integers in the range [1, 20]
+•	On the following lines, you will receive each row with its columns - integers, separated by a single space in the range [-20, 20]
+
+Output
+•	On the first line, print the maximum sum of the elements in the 3x3 square in the format "Sum = {sum}"
+•	On the following 3 lines, print each element of the found submatrix, separated by a single space
