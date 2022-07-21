efc5ea62c992697d4d52463b83cd92c7044c74f7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-21 09:15:29 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/4_maximal_sum.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/4_maximal_sum.py
@@ -8,3 +8,27 @@
 # Output
 # •	On the first line, print the maximum sum of the elements in the 3x3 square in the format "Sum = {sum}"
 # •	On the following 3 lines, print each element of the found submatrix, separated by a single space
+
+from sys import maxsize
+rows, columns = [int(el) for el in input().split()]
+
+matrix = []
+
+for _ in range(rows):
+    matrix.append([int(el) for el in input().split()])
+current_max = -maxsize
+best_nums = []
+for row_i in range(rows - 2):
+    for col_i in range(columns - 2):
+        current = [
+            matrix[row_i][col_i], matrix[row_i][col_i + 1], matrix[row_i][col_i + 2],
+            matrix[row_i + 1][col_i], matrix[row_i + 1][col_i + 1], matrix[row_i + 1][col_i + 2],
+            matrix[row_i + 2][col_i], matrix[row_i + 2][col_i + 1], matrix[row_i + 2][col_i + 2]
+        ]
+        if sum(current) > current_max:
+            current_max = sum(current)
+            best_nums = current.copy()
+print(f"Sum = {current_max}")
+print(f"{best_nums[0]} {best_nums[1]} {best_nums[2]}")
+print(f"{best_nums[3]} {best_nums[4]} {best_nums[5]}")
+print(f"{best_nums[6]} {best_nums[7]} {best_nums[8]}")
