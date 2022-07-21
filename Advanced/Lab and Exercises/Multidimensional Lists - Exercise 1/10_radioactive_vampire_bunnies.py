dbc57637b762ad96f0897b4613b9a112fda90ad3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-21 09:27:48 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/10_radioactive_vampire_bunnies.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/10_radioactive_vampire_bunnies.py
@@ -30,3 +30,92 @@
 # Constraints
 # •	The dimensions of the lair are in the range [3…20]
 # •	The directions string length is in the range [1…20]
+
+def multiply_bunnies(r_size, col_size):
+    global dead
+    new_bunnies = []
+    for r, j in rabbits:
+
+        if 0 <= r - 1 < r_size:
+            if matrix[r - 1][j] == "P":
+                dead = True
+            matrix[r - 1][j] = "B"
+            new_bunnies.append([r - 1, j])
+        if 0 <= r + 1 < r_size:
+            if matrix[r + 1][j] == "P":
+                dead = True
+            matrix[r + 1][j] = "B"
+            new_bunnies.append([r + 1, j])
+        if 0 <= j + 1 < col_size:
+            if matrix[r][j + 1] == "P":
+                dead = True
+            matrix[r][j + 1] = "B"
+            new_bunnies.append([r, j + 1])
+        if 0 <= j - 1 < col_size:
+            if matrix[r][j - 1] == "P":
+                dead = True
+            matrix[r][j - 1] = "B"
+            new_bunnies.append([r, j - 1])
+
+    return new_bunnies
+
+
+rows, columns = [int(el) for el in input().split()]
+
+matrix = []
+
+idx_row = 0
+idx_col = 0
+
+rabbits = []
+dead = False
+win = False
+
+for i1 in range(rows):
+    current = list(input())
+    for i2 in range(len(current)):
+        if current[i2] == "P":
+            idx_row = i1
+            idx_col = i2
+        elif current[i2] == "B":
+            position = [i1, i2]
+            rabbits.append(position)
+        else:
+            current[i2] = "."
+    matrix.append(current)
+# print(position)
+
+directions = list(input())
+
+# while still_alive and still_playing:
+last = [0, 0]
+for direction in directions:
+    last[0], last[1] = idx_row, idx_col
+    matrix[idx_row][idx_col] = "."
+    if direction == "R":
+        idx_col += 1
+    elif direction == "L":
+        idx_col -= 1
+    elif direction == "U":
+        idx_row -= 1
+    elif direction == "D":
+        idx_row += 1
+
+    if 0 > idx_row or idx_row >= rows or 0 > idx_col or idx_col >= columns:
+        win = True
+        matrix[last[0]][last[1]] = "."
+    elif matrix[idx_row][idx_col] == "B":
+        dead = True
+    else:
+        matrix[idx_row][idx_col] = "P"
+    rabbits = multiply_bunnies(rows, columns)
+    if dead or win:
+        break
+for row in matrix:
+    print(*row, sep="")
+
+if dead:
+    print(f"dead: {idx_row} {idx_col}")
+if win:
+    print(f"won: {last[0]} {last[1]}")
+
