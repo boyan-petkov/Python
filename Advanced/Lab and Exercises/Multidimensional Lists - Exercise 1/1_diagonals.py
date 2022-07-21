192931db4d9fdf2d699f354ff75ed9d1dd1ecf43
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-21 09:14:12 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/1_diagonals.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/1_diagonals.py
@@ -2,3 +2,25 @@
 # You should find the matrix's diagonals, prints them and their sum in the format:
 # "Primary diagonal: {element1}, {element2}, … {elementN}. Sum: {sum_of_primary}
 # Secondary diagonal: {element1}, {element2}, … {elementN}. Sum: {sum_of_secondary}".
+
+rows = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    nums = [int(el) for el in input().split(", ")]
+    matrix.append(nums)
+
+primary = []
+secondary = []
+
+for i in range(rows):
+    current = matrix[i][i]
+    primary.append(current)
+    current2 = matrix[i][rows - 1 - i]
+    secondary.append(current2)
+total = sum(primary)
+total2 = sum(secondary)
+
+print(f"Primary diagonal:", ', '.join([str(x) for x in primary]) + f". Sum: {total}")
+print(f"Secondary diagonal:", ', '.join([str(x) for x in secondary]) + f". Sum: {total2}")
