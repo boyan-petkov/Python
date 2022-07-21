1a4f962eeafc7ddff119f20a6cabf78b93572a25
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-21 09:26:09 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/6_matrix_shuffling.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/6_matrix_shuffling.py
@@ -8,3 +8,33 @@
 # •	If the command is not valid (does not contain the keyword "swap", has fewer or more coordinates entered, or the given coordinates are not valid), 
 # print "Invalid input!" and move on to the following command. A negative value makes the coordinates not valid.
 # Your program should finish when the command "END" is entered.
+
+rows, columns = [int(el) for el in input().split()]
+
+matrix = []
+
+for _ in range(rows):
+    matrix.append(input().split())
+
+command = input()
+
+while not command == "END":
+    cmd = command.split()
+    if cmd[0] == "swap" and len(cmd) == 5:
+        row1, col1, row2, col2, = int(cmd[1]), int(cmd[2]), int(cmd[3]), int(cmd[4])
+        if 0 <= row1 < rows and 0 <= col1 < columns and 0 <= row2 < rows and 0 <= col2 < columns:
+            first = matrix[row1][col1]
+            second = matrix[row2][col2]
+            matrix[row1][col1] = second
+            matrix[row2][col2] = first
+            for row_i in range(rows):
+                for col_i in range(columns):
+                    print(matrix[row_i][col_i],  end=" ")
+                print()
+
+        else:
+            print("Invalid input!")
+    else:
+        print("Invalid input!")
+
+    command = input()
