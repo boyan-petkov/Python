fb426dbfc3763a268ad48ed33f127344ac903f52
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-21 09:14:43 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/2_diagonal_difference.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 1/2_diagonal_difference.py
@@ -2,3 +2,24 @@
 # On the first line, you will receive an integer N - the size of a square matrix. 
 # The following N lines hold the values for each column - N numbers separated by a single space. 
 # Print the absolute difference between the primary and the secondary diagonal sums.
+
+rows = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    nums = [int(el) for el in input().split(" ")]
+    matrix.append(nums)
+
+primary = []
+secondary = []
+
+for i in range(rows):
+    current = matrix[i][i]
+    primary.append(current)
+    current2 = matrix[i][rows - 1 - i]
+    secondary.append(current2)
+total = sum(primary)
+total2 = sum(secondary)
+
+print(abs(total - total2))
