92d183a30246ba587bee4d97e25491bd6bd8d324
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-23 11:00:52 +0300

    Create knight_game.py

---
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 2/knight_game.py
@@ -0,0 +1,15 @@
+Chess is the oldest game, but it is still popular these days. It will be used only one chess piece for this task - the Knight. 
+A chess knight has 8 possible moves it can make, as illustrated. It can move to the nearest square but not on the same row, column,
+or diagonal. (e.g., it can move two squares horizontally, then one square vertically, 
+or it can move one square horizontally then two squares vertically - i.e., in an "L" pattern.) 
+The knight game is played on a board with dimensions N x N.
+You will receive a board with "K" for knights and "0" for empty cells. Your task is to remove knights until no knights that can attack 
+one another with one move are left.
+Input
+•	On the first line, you will receive integer N - the size of the board
+•	On the following N lines, you will receive strings with "K" and "0"
+Output
+•	Print a single integer with the minimum number of knights that need to be removed
+Constraints
+•	The size of the board will be 0 < N < 30
+•	Time limit: 0.3 sec. Memory limit: 16 MB
