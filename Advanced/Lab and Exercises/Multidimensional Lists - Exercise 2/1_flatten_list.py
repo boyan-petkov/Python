e1542d7daac260f7088d9951cd607dd7d11cd07f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-25 17:56:45 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Exercise 2/1_flatten_list.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 2/1_flatten_list.py
@@ -2,3 +2,25 @@
 # 	String with numbers or empty strings separated by "|"
 # 	Values are separated by spaces (" ", one or several)
 # 	Order the output list from the last to the first matrix sub-lists and their values from left to right as shown below
+
+raw_text = input().split("|",)
+
+digits = []
+
+for el in raw_text:
+    current = el.split()
+    digits.append(current)
+final = []
+for row_i in range(len(digits)):
+    start_from = len(digits) - (1 + row_i)
+    finish_on = len(digits[start_from])
+    for column in digits[start_from]:
+        column = column.split()
+        for char in column:
+            if char == " ":
+                continue
+            else:
+                final.append(char)
+print(*final, sep=" ")
+
+
