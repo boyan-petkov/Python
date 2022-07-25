ee36291c882f817e6c947147d00a5f68595b17d7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-25 17:57:20 +0300

    Initial commit

--- Advanced/Lab and Exercises/Multidimensional Lists - Exercise 2/2_knight_game.py
+++ Advanced/Lab and Exercises/Multidimensional Lists - Exercise 2/2_knight_game.py
@@ -16,3 +16,67 @@
 # Constraints
 # •	The size of the board will be 0 < N < 30
 # •	Time limit: 0.3 sec. Memory limit: 16 MB
+
+def is_valid(row, col, size):
+    if 0 <= row < size and 0 <= col < size:
+        return True
+
+
+def horse_there(row, col):
+    if matrix[row][col] == "K":
+        return True
+
+
+def can_attack(row, col, size):
+    counter = 0
+    if is_valid(row - 2, col - 1, size) and horse_there(row - 2, col - 1):
+        counter += 1
+    if is_valid(row - 2, col + 1, size) and horse_there(row - 2, col + 1):
+        counter += 1
+    if is_valid(row - 1, col - 2, size) and horse_there(row - 1, col - 2):
+        counter += 1
+    if is_valid(row - 1, col + 2, size) and horse_there(row - 1, col + 2):
+        counter += 1
+    if is_valid(row + 2, col - 1, size) and horse_there(row + 2, col - 1):
+        counter += 1
+    if is_valid(row + 2, col + 1, size) and horse_there(row + 2, col + 1):
+        counter += 1
+    if is_valid(row + 1, col - 2, size) and horse_there(row + 1, col - 2):
+        counter += 1
+    if is_valid(row + 1, col + 2, size) and horse_there(row + 1, col + 2):
+        counter += 1
+    return counter
+
+
+rows = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    matrix.append(list(input()))
+removed = 0
+while True:
+    table = {}
+    for row in range(rows):
+        for col in range(rows):
+            if matrix[row][col] == "0":
+                continue
+            counter_for_horse = can_attack(row, col, rows)
+            coordinates = (row, col)
+            if counter_for_horse > 0:
+                table[coordinates] = counter_for_horse
+    if not table:
+        break
+    best_counter = 0
+    horse_row = 0
+    horse_col = 0
+
+    for horse, count in table.items():
+        row, col = horse[0], horse[1]
+        if count > best_counter:
+            best_counter = count
+            horse_row, horse_col = row, col
+    matrix[horse_row][horse_col] = "0"
+    removed += 1
+
+print(removed)
