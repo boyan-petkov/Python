88ac6492c4623aaa6298da8498cb407cb6f55dee
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-26 16:17:57 +0300

    Update and rename operate.py to 4_operate.py

---
+++ Advanced/Lab and Exercises/Functions Advanced - Lab/4_operate.py
@@ -0,0 +1,4 @@
+# Write a function called operate that receives an operator ("+", "-", "*" or "/") as first argument and multiple numbers (integers) as additional arguments (*args). 
+# The function should return the result of the operator applied to all the numbers. For more clarification, see the examples below. 
+# Submit only your function in the Judge system.
+# Note: Be careful when you have multiplication and division
