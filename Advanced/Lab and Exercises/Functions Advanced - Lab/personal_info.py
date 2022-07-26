94dfdcc140f14cf0a82875d13e95814b54b94816
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-26 16:17:26 +0300

    Update and rename personal_info.py to 2_personal_info.py

--- Advanced/Lab and Exercises/Functions Advanced - Lab/personal_info.py
+++
@@ -1,3 +0,0 @@
-Write a function called get_info that receives a name, an age, and a town and returns a string in the format: 
-"This is {name} from {town} and he is {age} years old". Use dictionary unpacking when testing your function. 
-Submit only the function in the judge system.
