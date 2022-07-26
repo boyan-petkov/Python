95df874f0d82707440b428c5879ac2153dd9bd8c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-26 16:17:11 +0300

    Update and rename multiplication_function.py to 1_multiplication_function.py

--- Advanced/Lab and Exercises/Functions Advanced - Lab/multiplication_function.py
+++
@@ -1,2 +0,0 @@
-Write a function called multiply that can receive any quantity of numbers (integers) as different parameters and returns the result of the multiplication of all of them. 
-Submit only your function in the Judge system.
