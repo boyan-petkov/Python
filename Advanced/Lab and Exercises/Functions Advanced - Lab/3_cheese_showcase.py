a60812d19ea633fcfb9bfb3d2d539268a0145d7b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-27 19:14:35 +0300

    Initial commit

--- Advanced/Lab and Exercises/Functions Advanced - Lab/3_cheese_showcase.py
+++ Advanced/Lab and Exercises/Functions Advanced - Lab/3_cheese_showcase.py
@@ -5,3 +5,21 @@
 # If two or more cheeses have the same number of pieces, sort them by their names in ascending order (alphabetically). 
 # For each kind of cheese, return their pieces quantities in descending order.
 # For more clarifications, see the examples below.
+
+def sorting_cheeses(**kwargs):
+    result = ""
+    sorted_cheese = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))
+    for tuple_ in sorted_cheese:
+        final = sorted(tuple_[1], reverse= True )
+        # return (tuple_[0]), "\n".join([str(el) for el in final])
+        result += tuple_[0] + "\n" + "\n".join([str(el) for el in final]) + "\n"
+        # result = result.strip()
+    return result
+
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)
