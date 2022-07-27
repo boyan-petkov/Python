ad67a5f8a6b8654efda70034397c4b4578fa1bfa
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-27 19:15:37 +0300

    Initial commit

--- Advanced/Lab and Exercises/Functions Advanced - Lab/5_recursive_power.py
+++ Advanced/Lab and Exercises/Functions Advanced - Lab/5_recursive_power.py
@@ -1,3 +1,9 @@
 # Create a recursive function called recursive_power() which should receive a number and a power.
 # Using recursion, return the result of number ** power. Submit only the function in the judge system.
 
+def recursive_power(num, power):
+    result = 1
+    if power == 0:
+        return result
+    result = num * recursive_power(num, power - 1)
+    return result
