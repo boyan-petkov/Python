b38213be1d81b9e3a19e5b75b4b2ffe5704703b3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-27 19:15:09 +0300

    Initial commit

--- Advanced/Lab and Exercises/Functions Advanced - Lab/4_operate.py
+++ Advanced/Lab and Exercises/Functions Advanced - Lab/4_operate.py
@@ -2,3 +2,24 @@
 # The function should return the result of the operator applied to all the numbers. For more clarification, see the examples below. 
 # Submit only your function in the Judge system.
 # Note: Be careful when you have multiplication and division
+
+def operate(operator, *args):
+    result = 0
+    if operator in "-+":
+        result = 0
+    else:
+        result = 1
+
+    if operator == "+":
+        for num in args:
+            result += num
+    elif operator == "-":
+        for num in args:
+            result -= num
+    elif operator == "/":
+        for num in args:
+            result /= num
+    elif operator == "*":
+        for num in args:
+            result *= num
+    return result
