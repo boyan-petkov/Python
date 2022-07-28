46366a7f02b09f27ba3de5948c087dade06b1661
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-28 16:24:22 +0300

    Update and rename keywoord_arguments_length.py to 2_keywoord_arguments_length.py

---
+++ Advanced/Lab and Exercises/Functions Advanced - Exercise/2_keywoord_arguments_length.py
@@ -0,0 +1,3 @@
+# Create a function called kwargs_length() that can receive some keyword arguments and return their length.
+# Submit only your function in the judge system.
+
--- Advanced/Lab and Exercises/Functions Advanced - Exercise/keywoord_arguments_length.py
+++
@@ -1,2 +0,0 @@
-Create a function called kwargs_length() that can receive some keyword arguments and return their length.
-Submit only your function in the judge system.
