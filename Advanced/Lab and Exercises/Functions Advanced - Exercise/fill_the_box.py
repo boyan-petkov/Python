b0fb6fd25ae2aeaedd5035e2478c87e90d37e86b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-28 16:23:01 +0300

    Create fill_the_box.py

---
+++ Advanced/Lab and Exercises/Functions Advanced - Exercise/fill_the_box.py
@@ -0,0 +1,16 @@
+Write a function called fill_the_box that receives a different number of arguments representing:
+the height of a box
+the length of a box
+the width of a box
+n-times a different number of cubes with exact size 1 x 1 x 1
+a string "Finish"
+Your task is to fill the box with the given cubes until the current argument equals "Finish".
+Note: Submit only the function in the judge system
+Input
+There will be no input. Just parameters passed to your function.
+Output
+The function should return a string in the following format:
+If, at the end, there is free space left in the box, print:
+"There is free space in the box. You could put {free space in cubes} more cubes."
+If there is no free space in the box, print:
+"No more free space! You have {cubes left} more cubes."
