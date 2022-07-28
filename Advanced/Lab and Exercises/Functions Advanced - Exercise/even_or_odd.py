90c511398ebb655b246dbf8d8ec64bebe4db1bce
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-28 16:24:38 +0300

    Update and rename even_or_odd.py to 3_even_or_odd.py

--- Advanced/Lab and Exercises/Functions Advanced - Exercise/even_or_odd.py
+++
@@ -1,3 +0,0 @@
-Create a function called even_odd() that can receive a different quantity of numbers and a command at the end.
-The command can be "even" or "odd". Filter the numbers depending on the command and return them in a list. Submit only the function in the judge system.
-Submit only your function in the judge system.
