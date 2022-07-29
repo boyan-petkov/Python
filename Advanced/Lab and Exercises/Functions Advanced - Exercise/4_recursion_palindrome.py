1a1314de7de554bedbf675d5413c5194f7a46900
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-07-29 18:56:28 +0300

    Initial commit

--- Advanced/Lab and Exercises/Functions Advanced - Exercise/4_recursion_palindrome.py
+++ Advanced/Lab and Exercises/Functions Advanced - Exercise/4_recursion_palindrome.py
@@ -2,3 +2,20 @@
 # Implement the function, so it returns "{word} is a palindrome" if the word is a palindrome and "{word} is not a palindrome" 
 # if the word is not a palindrome using recursion. Submit only the function in the judge system.
 
+
+def palindrome(*args):
+    text, idx = args[0], int(args[1])
+    if idx == len(text) // 2:
+        return f"{text} is a palindrome"
+
+    left = text[idx]
+    right = text[len(text) - 1 - idx]
+    
+    if left != right:
+        return f"{text} is not a palindrome"
+
+    return palindrome(text, idx + 1)
+
+
+print(palindrome("peter", 0))
+print(palindrome("abcba", 0))
