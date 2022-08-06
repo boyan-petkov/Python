fd7445d4bdfa79298106fbbad1ce20ee2a78ac91
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-06 12:25:24 +0300

    Initial commit

--- Advanced/Exam/19_Feb_2022/3_spring_time.py
+++ Advanced/Exam/19_Feb_2022/3_spring_time.py
@@ -24,3 +24,47 @@
 # {type_N}:
 # …
 # -{last_spring_object_of_typeN}"
+
+def start_spring(**kwargs):
+    result = ""
+    spring_objects = {}
+    for key, value in kwargs.items():
+        if value not in spring_objects:
+            spring_objects[value] = []
+        spring_objects[value].append(key)
+
+    sorted_collections_and_objects = sorted(spring_objects.items(), key=lambda x: (-len(x[1]), x[0]))
+    for tuple in sorted_collections_and_objects:
+        type = tuple[0]
+        element  = tuple[1]
+        elements  = sorted(element)
+        result += f"{type}:\n"
+        for el in elements:
+            result += f"-{el}\n"
+    return result
+
+
+
+
+example_objects = {"Water Lilly": "flower",
+                   "Swifts": "bird",
+                   "Callery Pear": "tree",
+                   "Swallows": "bird",
+                   "Dahlia": "flower",
+                   "Tulip": "flower", }
+print(start_spring(**example_objects))
+example_objects = {"Swallow": "bird",
+                   "Thrushes": "bird",
+                   "Woodpeckers": "bird",
+                   "Swallows": "bird",
+                   "Warblers": "bird",
+                   "Shrikes": "bird", }
+print(start_spring(**example_objects))
+example_objects = {"Magnolia": "tree",
+                   "Swallow": "bird",
+                   "Thrushes": "bird",
+                   "Pear": "tree",
+                   "Cherries": "tree",
+                   "Shrikes": "bird",
+                   "Butterfly": "insect"}
+print(start_spring(**example_objects))
