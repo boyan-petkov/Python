ea6b9b5a31eaa5e1bf8770d640b6379981cb09dd
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-10 10:28:27 +0300

    Update music.py

--- OOP/Lab and Exercises/First Steps in OOP - Lab/music.py
+++ OOP/Lab and Exercises/First Steps in OOP - Lab/music.py
@@ -1,1 +1,6 @@
 
+Create class named Music that receives title (string), artist (string) and lyrics (string) upon initialization. 
+The class should also have methods print_info() and play():
+•	The print_info() method should return the following: 'This is "{title}" from "{artist}"'
+•	The play() method should return the lyrics.
+Submit only the class in the judge system. Test your code with your own examples.
