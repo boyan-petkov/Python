cfd183e8d0be72b0ec1d35c5264e2f4d6cd899ac
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-10 10:28:05 +0300

    Update car.py

--- OOP/Lab and Exercises/First Steps in OOP - Lab/car.py
+++ OOP/Lab and Exercises/First Steps in OOP - Lab/car.py
@@ -1,1 +1,4 @@
 
+Create a class called Car. Upon initialization it should receive a name, model and engine (all strings).
+Create a method called get_info() which will return a string in the following format: 
+"This is {name} {model} with engine {engine}".
