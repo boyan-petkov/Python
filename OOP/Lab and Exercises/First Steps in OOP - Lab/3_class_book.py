0216d78097963da45c213cda80464bb2ecd9e0d2
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-12 19:04:43 +0300

    Initial commit

--- OOP/Lab and Exercises/First Steps in OOP - Lab/3_class_book.py
+++ OOP/Lab and Exercises/First Steps in OOP - Lab/3_class_book.py
@@ -4,3 +4,15 @@
 # •	author: string
 # •	pages: int
 # Submit only the class in the judge system. 
+
+class Book:
+    def __init__(self, name, author, pages: int):
+        self.name = name
+        self.author = author
+        self.pages = pages
+
+
+book = Book("My Book", "Me", 200)
+print(book.name)
+print(book.author)
+print(book.pages)
