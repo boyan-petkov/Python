3fd2064a2951ae2e9a76e5f4ed9346354bf7d9c0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-12 19:05:49 +0300

    Initial commit

--- OOP/Lab and Exercises/First Steps in OOP - Lab/5_music.py
+++ OOP/Lab and Exercises/First Steps in OOP - Lab/5_music.py
@@ -4,3 +4,19 @@
 # •	The print_info() method should return the following: 'This is "{title}" from "{artist}"'
 # •	The play() method should return the lyrics.
 # Submit only the class in the judge system. Test your code with your own examples.
+
+class Music:
+    def __init__(self, title, artist, lyrics):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self):
+        return f'This is "{self.title}" from "{self.artist}"'
+
+    def play(self):
+        return self.lyrics
+
+song = Music("Title", "Artist", "Lyrics")
+print(song.print_info())
+print(song.play())
