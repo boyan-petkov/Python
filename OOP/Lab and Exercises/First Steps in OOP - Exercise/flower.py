a43c2267f7e43b1920db92bfcb6b90bf2996eb43
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-13 11:03:53 +0300

    Update and rename flower.py to 5_flower.py

--- OOP/Lab and Exercises/First Steps in OOP - Exercise/flower.py
+++
@@ -1,7 +0,0 @@
-Create a class called Flower. Upon initialization, the class should receive name (string) and water_requirements (number).
-The flower should also have an instance attribute called is_happy (False by default). Add two methods to the class:
--	water(quantity) - it will water the flower. Each time check if the quantity is greater than or equal to the required.
-If it is - the flower becomes happy (set is_happy to True). 
--	status() - it should return "{name} is happy" if the flower is happy, otherwise it should return "{name} is not happy". 
-Submit only the class in the judge system.
-
