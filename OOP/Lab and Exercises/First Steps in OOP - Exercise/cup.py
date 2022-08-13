fa92b2293b9741da95fa86b75f26733adbef358f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-13 11:00:15 +0300

    Update cup.py

--- OOP/Lab and Exercises/First Steps in OOP - Exercise/cup.py
+++ OOP/Lab and Exercises/First Steps in OOP - Exercise/cup.py
@@ -1,1 +1,6 @@
+Create a class called Cup. Upon initialization it should receive size (number) and quantity (a number representing how much liquid is in it). 
+The class should have two methods:
+•	fill(milliliters) which will increase the amount of liquid in the cup with the given milliliters (if there is space in the cup, otherwise ignore). 
+•	status() which will return the amount of free space left in the cup. 
+Submit only the class in the judge system. Do not forget to test your code.
 
