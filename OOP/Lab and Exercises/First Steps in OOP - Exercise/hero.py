c3b2281e58ced61fd557b6b463ad3e52a8438dd6
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-13 11:02:56 +0300

    Update and rename hero.py to 2_hero.py

--- OOP/Lab and Exercises/First Steps in OOP - Exercise/hero.py
+++
@@ -1,6 +0,0 @@
-Create a class called Hero. Upon initialization it should receive a name (string) and health (number). Create two methods:
-•	defend(damage) - reduce the given damage from the hero's health:
-o	if the health become 0 or less, set it to 0 and return "{name} was defeated"
-•	heal(amount) - increase the health of the hero with the given amount
-Examples
-
