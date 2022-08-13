068a299373161e31e5d419322c227cdcd4b2a566
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-13 11:03:18 +0300

    Update and rename employee.py to 3_employee.py

--- OOP/Lab and Exercises/First Steps in OOP - Exercise/employee.py
+++
@@ -1,6 +0,0 @@
-Create class Employee. Upon initialization, it should receive id (number), first_name (string), last_name (string) and salary (number).
-Create 3 instance methods:
--	get_full_name() - returns "{first_name} {last_name}"
--	get_annual_salary() - returns the total salary for 12 months
--	raise_salary(amount) - increases the salary by the given amount and returns the new salary
-
