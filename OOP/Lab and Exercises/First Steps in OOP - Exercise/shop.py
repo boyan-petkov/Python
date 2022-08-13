7ab8c8fe08a0b489a09d882c4d171907dc9d007b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-13 11:02:27 +0300

    Update and rename shop.py to 1_shop.py

--- OOP/Lab and Exercises/First Steps in OOP - Exercise/shop.py
+++
@@ -1,2 +0,0 @@
-Create a class called Shop. Upon initialization it should receive a name (string) and items (list). 
-Create a method called get_items_count() which should return the number of items in the store.
