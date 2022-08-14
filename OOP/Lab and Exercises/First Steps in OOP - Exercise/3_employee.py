9e225bef832cfdf93402d8c7bb2b829212f067e7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-14 08:17:49 +0300

    Initial commit

--- OOP/Lab and Exercises/First Steps in OOP - Exercise/3_employee.py
+++ OOP/Lab and Exercises/First Steps in OOP - Exercise/3_employee.py
@@ -4,3 +4,25 @@
 # -	get_annual_salary() - returns the total salary for 12 months
 # -	raise_salary(amount) - increases the salary by the given amount and returns the new salary
 
+class Employee:
+    def __init__(self, id, first_name, last_name, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_annual_salary(self):
+        return 12 * self.salary
+
+    def raise_salary(self, amount):
+        self.salary += amount
+        return self.salary
+
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
