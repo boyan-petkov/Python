33296703fc6e7e9031fea7e78a38cad897f9a51e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-14 08:20:09 +0300

    Initial commit

--- OOP/Lab and Exercises/First Steps in OOP - Exercise/7_programmer.py
+++ OOP/Lab and Exercises/First Steps in OOP - Exercise/7_programmer.py
@@ -11,3 +11,32 @@
 # o	In the last case the programmer does not have the skills, so return "{name} needs {needed_skills} more skills" and do not change his language
 # Submit only the class in the judge system.
 
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if self.language == language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if self.skills >= skills_needed and self.language != new_language:
+            temp = self.language
+            self.language = new_language
+            return f"{self.name} switched from {temp} to {new_language}"
+        elif self.skills >= skills_needed and self.language == new_language:
+            return f"{self.name} already knows {self.language}"
+        elif self.skills < skills_needed:
+            return f"{self.name} needs {abs(self.skills - skills_needed)} more skills"
+
+programmer = Programmer("John", "Java", 50)
+print(programmer.watch_course("Python Masterclass", "Python", 84))
+print(programmer.change_language("Java", 30))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Java: zero to hero", "Java", 50))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Python Masterclass", "Python", 84))
