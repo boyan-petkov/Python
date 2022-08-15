d72228852884666ccf533824126d34d44815d1db
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-15 10:48:38 +0300

    Update and rename point.py to 2_point.py

--- OOP/Lab and Exercises/Classes and Objects - Lab/point.py
+++
@@ -1,5 +0,0 @@
-Create a class called Point. Upon initialization it should receive x and y (numbers). Create 3 instance methods:
--	set_x(new_x) - changes the x value of the point
--	set_y(new_y) - changes the y value of the point
--	__str__ - returns the coordinates of the point in the format "The point has coordinates ({x},{y}) "
-
