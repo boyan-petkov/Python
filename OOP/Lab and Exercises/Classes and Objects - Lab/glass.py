3cc6437e977d533afa0b0d190253180d23cb2474
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-15 10:49:20 +0300

    Update and rename glass.py to 4_glass.py

--- OOP/Lab and Exercises/Classes and Objects - Lab/glass.py
+++
@@ -1,7 +0,0 @@
-Create a class called Glass. Upon initialization it will not receive any parameters,
-you must create however an instance attribute called content which should be equal to 0. You should also create a class attribute called capacity which should be 250 ml. 
-Create 3 instance methods:
--	fill(ml) - fill the glass with the given milliliters if there is enough space in it and return "Glass filled with {ml} ml", otherwise return "Cannot add {ml} ml"
--	empty() - empty the glass and return "Glass is now empty" 
--	info() - returns info about the glass in the format "{space_left} ml left"
-
