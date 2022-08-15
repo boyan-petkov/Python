ef4823484be375a9a23bba9c70abb5d35b830d7c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-15 10:48:24 +0300

    Update and rename Vehicle.py to 1_vehicle.py

--- OOP/Lab and Exercises/Classes and Objects - Lab/Vehicle.py
+++
@@ -1,2 +0,0 @@
-Create a class called Vehicle. Upon initialization it should receive max_speed (number, optional; 150 by default) and mileage (number).
-Create an instance variable called gadgets – empty list by default.
