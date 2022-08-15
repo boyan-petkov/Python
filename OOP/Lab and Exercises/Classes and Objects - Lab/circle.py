825276099576127c756dd7b58c9f50df6dd224e0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-15 10:46:55 +0300

    Update circle.py

--- OOP/Lab and Exercises/Classes and Objects - Lab/circle.py
+++ OOP/Lab and Exercises/Classes and Objects - Lab/circle.py
@@ -1,1 +1,5 @@
 
+Create a class called Circle. Upon initialization it should receive a radius (number). Create a class attribute called pi which should be equal to 3.14. Create 3 instance methods:
+-	set_radius(new_radius) - changes the radius
+-	get_area() - returns the area of the circle
+-	get_circumference() - returns the circumference of the circle
