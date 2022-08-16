848f2e915da93827935c2e9b50e9681a6c01071c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-16 10:54:24 +0300

    Update 5_smartphone.py

--- OOP/Lab and Exercises/Classes and Objects - Lab/5_smartphone.py
+++ OOP/Lab and Exercises/Classes and Objects - Lab/5_smartphone.py
@@ -6,4 +6,32 @@
 # o	If there is enough memory, but the phone is off, return "Turn on your phone to install {app}"
 # o	Otherwise return "Not enough memory to install {app}"
 # -	status() - returns "Total apps: {total_apps_count}. Memory left: {memory_left}"
+class Smartphone:
+    def __init__(self, memory):
+        self.memory = memory
+        self.apps = []
+        self.is_on = False
 
+    def power(self):
+        self.is_on = True if self.is_on is False else False
+
+    def install(self, app, app_memory):
+        if self.is_on is True and self.memory >= app_memory:
+            self.apps.append(app)
+            self.memory -= app_memory
+            return f"Installing {app}"
+        elif self.is_on is False and self.memory >= app_memory:
+            return f"Turn on your phone to install {app}"
+        return f"Not enough memory to install {app}"
+
+    def status(self):
+
+        return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
+
+smartphone = Smartphone(100)
+print(smartphone.install("Facebook", 60))
+smartphone.power()
+print(smartphone.install("Facebook", 60))
+print(smartphone.install("Messenger", 20))
+print(smartphone.install("Instagram", 40))
+print(smartphone.status())
