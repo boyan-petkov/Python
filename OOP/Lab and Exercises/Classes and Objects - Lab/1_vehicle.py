84b3747c8c701a180669b7d6def61be4ba182423
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-16 10:55:01 +0300

    Initial commit

--- OOP/Lab and Exercises/Classes and Objects - Lab/1_vehicle.py
+++ OOP/Lab and Exercises/Classes and Objects - Lab/1_vehicle.py
@@ -1,11 +1,14 @@
 # Create a class called Vehicle. Upon initialization it should receive max_speed (number, optional; 150 by default) and mileage (number).
 # Create an instance variable called gadgets – empty list by default.
 
+
 class Vehicle:
     def __init__(self, mileage: int, max_speed=150):
         self.mileage = mileage
         self.max_speed = max_speed
         self.gadgets = []
+
+
 car = Vehicle(20)
 print(car.max_speed)
 print(car.mileage)
