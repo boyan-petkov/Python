5d0a4f75d74ff4586b6fbbabbf9fb69f05249fed
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-17 11:55:30 +0300

    Update time.py

--- OOP/Lab and Exercises/Classes and Objects - Exercise/time.py
+++ OOP/Lab and Exercises/Classes and Objects - Exercise/time.py
@@ -1,1 +1,5 @@
+Create a class called Time. Upon initialization, it should receive hours, minutes, and seconds (integers). The class should also have class attributes max_hours equal to 23, max_minutes equal to 59, and max_seconds equal to 59. You should also create 3 additional instance methods:
+-	set_time(hours, minutes, seconds) - updates the time with the new values
+-	get_time() - returns "{hh}:{mm}:{ss}"
+-	next_second() - updates the time with one second (use the class attributes for validation) and returns the new time (use the get_time() method)
 
