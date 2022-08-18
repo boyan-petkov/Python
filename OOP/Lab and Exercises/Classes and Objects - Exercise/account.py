5b6411ab8c0ad252a10c74a5d551ab92b222ed95
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-18 14:15:16 +0300

    Update and rename account.py to 3_account.py

--- OOP/Lab and Exercises/Classes and Objects - Exercise/account.py
+++
@@ -1,5 +0,0 @@
-
-Create a class called Account. Upon initialization, it should receive an id (number), a name (string), and a balance (integer; optional; 0 by default). The class should also have 3 additional instance methods:
--	credit(amount) - adds the amount to the balance and returns the new balance
--	debit(amount) - if the amount is less than or equal to the balance, reduces the balance by the amount and returns the new balance. Otherwise, return "Amount exceeded balance"
--	info() - returns "User {name} with account {id} has {balance} balance"
