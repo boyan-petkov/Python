ff977ffc8c217b76c0b7315d88ced5a2aa9fc5a6
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-18 14:19:56 +0300

    Initial commit

--- OOP/Lab and Exercises/Classes and Objects - Exercise/1_vet.py
+++ OOP/Lab and Exercises/Classes and Objects - Exercise/1_vet.py
@@ -8,3 +8,44 @@
 # -	info()
 # o	Returns info about the vet, the number of animals in his list and the free space in the clinic:
 # "{vet_name} has {number_animals} animals. {space_left_in_clinic} space left in clinic"
+
+class Vet:
+    animals = []
+    space = 5
+
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name):
+        if len(Vet.animals) == Vet.space:
+            return "Not enough space"
+        self.animals.append(animal_name)
+        Vet.animals.append(animal_name)
+        return f"{animal_name} registered in the clinic"
+
+    def unregister_animal(self, animal_name):
+        if animal_name in self.animals:
+            Vet.animals.remove(animal_name)
+            self.animals.remove(animal_name)
+            return f"{animal_name} unregistered successfully"
+        return f"{animal_name} not in the clinic"
+
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space - len(Vet.animals)} space left in clinic"
+
+
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.register_animal("Deyan"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
