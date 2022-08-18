30c2d9f7870bcdcea90babc70b01b56af403c9e0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-18 14:28:34 +0300

    Initial commit

--- OOP/Lab and Exercises/Classes and Objects - Exercise/6_guild_system/player.py
+++ OOP/Lab and Exercises/Classes and Objects - Exercise/6_guild_system/player.py
@@ -1,1 +1,20 @@
+class Player:
+    default_guild = "Unaffiliated"
+
+    def __init__(self, name, hp, mp):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = Player.default_guild
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return "Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        result = '\n'.join([f"==={skill} - {mana}" for skill, mana in self.skills.items()])
+        return f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n" + result + "\n"
 
