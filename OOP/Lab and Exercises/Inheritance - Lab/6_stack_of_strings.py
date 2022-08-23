263b2e0788eb04fe481392efdf902f5b7905f1c3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-23 15:18:08 +0300

    Initial commit

--- OOP/Lab and Exercises/Inheritance - Lab/6_stack_of_strings.py
+++ OOP/Lab and Exercises/Inheritance - Lab/6_stack_of_strings.py
@@ -7,3 +7,23 @@
 # •	Override the string method to return the stack data in the format: 
 # "[{element(N)}, {element(N-1)} ... {element(0)}]"
 
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, element):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return f"{self.data[-1]}"
+
+    def is_empty(self):
+        if not self.data:
+            return True
+        return False
+
+    def __str__(self):
+        return "[" + ", ".join(reversed(self.data)) + "]"
