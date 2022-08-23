a43d06a8a1b90d4eea61cfdd40a5abdeb623cf69
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-23 15:09:48 +0300

    Update 6_stack_of_strings.py

--- OOP/Lab and Exercises/Inheritance - Lab/6_stack_of_strings.py
+++ OOP/Lab and Exercises/Inheritance - Lab/6_stack_of_strings.py
@@ -1,1 +1,9 @@
+# Create a class Stack which can store only strings and has the following functionality:
+# •	Instance attribute: data: list
+# •	Method: push(element) – adds an element at the end of the stack
+# •	Method: pop() – removes and returns the last element in the stack
+# •	Method: top() - returns a reference to the top most element of the stack
+# •	Method: is_empty() - returns boolean True/False
+# •	Override the string method to return the stack data in the format: 
+# "[{element(N)}, {element(N-1)} ... {element(0)}]"
 
