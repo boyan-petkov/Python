0966257dff81d5b4b272eb61657912db10e6a637
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-08-31 16:32:28 +0300

    Initial commit

--- OOP/Lab and Exercises/Inheritance - Exercise/4_need_for_speed/project/motorcycle.py
+++ OOP/Lab and Exercises/Inheritance - Exercise/4_need_for_speed/project/motorcycle.py
@@ -1,1 +1,9 @@
+from project.vehicle import Vehicle
+
+
+class Motorcycle(Vehicle):
+
+    def __init__(self, fuel: float, horse_power: int):
+        super().__init__(fuel, horse_power)
+        # self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
 
