051e1ac308a58a9c21ebe5e5ac3be1cdf2994927
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-01 10:55:59 +0300

    Update 5_account.py

--- OOP/Lab and Exercises/Encapsulation - Lab/5_account.py
+++ OOP/Lab and Exercises/Encapsulation - Lab/5_account.py
@@ -1,1 +1,4 @@
+Create a class called Account. Upon initialization it should receive an id, a balance, and a pin (all numbers). The pin and the id should be private instance attributes and the balance should be public attribute. Create two public instance methods:
+-	get_id(pin) - if the given pin is correct, return the id, otherwise return "Wrong pin"
+-	change_pin(old_pin, new_pin) - if the old pin is correct, change it to the new one and return "Pin changed", otherwise return "Wrong pin"
 
