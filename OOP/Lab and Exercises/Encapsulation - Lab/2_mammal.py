94f1b4e658a46eecfd96d98b1101ce7aee196c54
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-05 19:31:03 +0300

    Update 2_mammal.py

--- OOP/Lab and Exercises/Encapsulation - Lab/2_mammal.py
+++ OOP/Lab and Exercises/Encapsulation - Lab/2_mammal.py
@@ -1,4 +1,6 @@
-Create a class called Mammal. Upon initialization it should receive a name, type and sound. Create class attribute called kingdom which should not be accessed outside the class and set it to be "animals". Create three more instance methods:
+Create a class called Mammal. Upon initialization it should receive a name, type and sound. 
+Create class attribute called kingdom which should not be accessed outside the class and set it to be "animals". 
+Create three more instance methods:
 -	make_sound() - returns a string in the format "{name} makes {sound}"
 -	get_kingdom() - returns the private kingdom attribute
 -	info() - returns a string in the format "{name} is of type {type}"
