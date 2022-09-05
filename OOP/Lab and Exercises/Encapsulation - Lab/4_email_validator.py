cc08545ae9d30e08c743cf30ae857dea8418a645
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-05 19:34:46 +0300

    Initial commit

--- OOP/Lab and Exercises/Encapsulation - Lab/4_email_validator.py
+++ OOP/Lab and Exercises/Encapsulation - Lab/4_email_validator.py
@@ -11,3 +11,23 @@
 # Create one public method:
 # -	validate(email) - using the three methods returns whether the email is valid (True/False)
 
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        username, mail_domain = email.split('@')
+        mail, domain = mail_domain.split('.')
+        return self.__is_name_valid(username) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
+         
