4bda8dc98c42e61b75c3e5126da04909fbe253c5
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-05 19:32:13 +0300

    Update 3_profile.py

--- OOP/Lab and Exercises/Encapsulation - Lab/3_profile.py
+++ OOP/Lab and Exercises/Encapsulation - Lab/3_profile.py
@@ -1,9 +1,9 @@
-Create a class called Profile. Upon initialization it should receive:
-•	username: str - the username should be between 5 and 15 characters (inclusive). 
-  If it is not, raise a ValueError with message "The username must be between 5 and 15 characters."
-•	password: str - the password must be at least 8 characters long; it must contain at least one upper case letter and at least one digit. 
-  If it does not, raise a ValueError with message "The password must be 8 or more characters with at least 1 digit and 1 uppercase letter."
-Hint: Use Getters and Setters to name mangle them. 
-Override the __str__() method of the base class so it returns: "You have a profile with username: "{username}" and password: 
-  {"*" with the length of password}".
+# Create a class called Profile. Upon initialization it should receive:
+# •	username: str - the username should be between 5 and 15 characters (inclusive). 
+#   If it is not, raise a ValueError with message "The username must be between 5 and 15 characters."
+# •	password: str - the password must be at least 8 characters long; it must contain at least one upper case letter and at least one digit. 
+#   If it does not, raise a ValueError with message "The password must be 8 or more characters with at least 1 digit and 1 uppercase letter."
+# Hint: Use Getters and Setters to name mangle them. 
+# Override the __str__() method of the base class so it returns: "You have a profile with username: "{username}" and password: 
+#   {"*" with the length of password}".
 
