bc02272270ee747497738e971d683a289c8218a8
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-05 19:30:46 +0300

    Update 1_person.py

--- OOP/Lab and Exercises/Encapsulation - Lab/1_person.py
+++ OOP/Lab and Exercises/Encapsulation - Lab/1_person.py
@@ -1,1 +1,3 @@
-Create a class called Person. Upon initialization it should receive name and age. Name mangle the name and the age attributes (should not be accessed outside the class). Create two instance methods called get_name and get_age to return the values of the private attributes.
+# Create a class called Person. Upon initialization it should receive name and age. 
+# Name mangle the name and the age attributes (should not be accessed outside the class). 
+# Create two instance methods called get_name and get_age to return the values of the private attributes.
