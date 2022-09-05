c788d7e0c9cab12243bb62c0cf56d02bcd7a456a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-05 19:33:40 +0300

    Initial commit

--- OOP/Lab and Exercises/Encapsulation - Lab/2_mammal.py
+++ OOP/Lab and Exercises/Encapsulation - Lab/2_mammal.py
@@ -5,3 +5,25 @@
 # -	get_kingdom() - returns the private kingdom attribute
 # -	info() - returns a string in the format "{name} is of type {type}"
 
+class Mammal:
+    __kingdom = "animals"
+
+    def __init__(self, name, type, sound):
+        self.name = name
+        self.type = type
+        self.sound = sound
+
+    def make_sound(self):
+        return f"{self.name} makes {self.sound}"
+
+    def get_kingdom(self):
+        return Mammal.__kingdom
+
+    def info(self):
+        return f"{self.name} makes {self.sound}"
+
+
+mammal = Mammal("Dog", "Domestic", "Bark")
+print(mammal.make_sound())
+print(mammal.get_kingdom())
+print(mammal.info())
