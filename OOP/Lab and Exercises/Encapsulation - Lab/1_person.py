114b1c8a5e5e84e2e29589769b928371495fa574
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-05 19:32:50 +0300

    Update 1_person.py

--- OOP/Lab and Exercises/Encapsulation - Lab/1_person.py
+++ OOP/Lab and Exercises/Encapsulation - Lab/1_person.py
@@ -1,3 +1,21 @@
 # Create a class called Person. Upon initialization it should receive name and age. 
 # Name mangle the name and the age attributes (should not be accessed outside the class). 
 # Create two instance methods called get_name and get_age to return the values of the private attributes.
+
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+
+person = Person("George", 32)
+print(person.get_name())
+print(person.get_age())
+person.__name = "Boyan"
+print(person.get_name())
