bed0760a7112ba6481909e839eb2379a24e02d3b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-09 21:07:04 +0300

    Initial commit

--- OOP/Lab and Exercises/Encapsulation - Exercise/3_football_team_generator/project/team.py
+++ OOP/Lab and Exercises/Encapsulation - Exercise/3_football_team_generator/project/team.py
@@ -1,1 +1,24 @@
 
+from project.player import Player
+
+class Team:
+
+    def __init__(self, name, rating):
+        self.__name = name
+        self.__rating = rating
+        self.__players = []
+
+    def add_player(self, player: Player):
+        for curr_player in self.__players:
+            if curr_player.name == player.name:
+                return f"Player {player.name} has already joined"
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name):
+        for player in self.__players:
+            if player_name == player.name:
+
+                self.__players.remove(player)
+                return player
+        return f"Player {player_name} not found"
