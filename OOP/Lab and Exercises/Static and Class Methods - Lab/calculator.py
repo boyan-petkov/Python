a97817bb87f698ef7c76ec3bb666318f08d57992
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-12 16:00:32 +0300

    Initial commit

--- OOP/Lab and Exercises/Static and Class Methods - Lab/calculator.py
+++ OOP/Lab and Exercises/Static and Class Methods - Lab/calculator.py
@@ -1,3 +1,10 @@
+# Create a class called Calculator that has the following static methods:
+# •	add(*args) - sums all the arguments passed to the function and returns the result
+# •	multiply(*args) - multiplies all the numbers and returns the result
+# •	divide(*args) - divides all the numbers (starting from the first one) and returns the result
+# •	subtract(*args) - subtracts all the numbers (starting from the first one) and returns the result
+
+
 from functools import reduce
 
 class Calculator:
