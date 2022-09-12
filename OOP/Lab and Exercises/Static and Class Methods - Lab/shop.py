7175b826bef74e26ad09bb7c97bb15d5b63f988b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-12 16:04:30 +0300

    Initial commit

--- OOP/Lab and Exercises/Static and Class Methods - Lab/shop.py
+++ OOP/Lab and Exercises/Static and Class Methods - Lab/shop.py
@@ -1,3 +1,14 @@
+# Create a class called Shop. Upon initialization, it should receive a name (str), type (str), capacity (int). 
+# The store should also have an attribute called items (an empty dictionary that stores the name of an item and its quantity). The class should have 4 methods:
+# •	small_shop(name: str, type: str) - a new shop with a capacity of 10 should be created
+# •	add_item(item_name:str) - adds 1 to the quantity of the given item. On success, the method should return "{item_name} added to the shop".
+# If the addition is not possible, the following message should be returned "Not enough capacity in the shop"
+# •	remove_item(item_name:str, amount:int) - removes the given amount from the item. On success, 
+# it should return "{amount} {item_name} removed from the shop". Otherwise, the method should return "Cannot remove {amount} {item_name}"
+# o	If the item quantity reaches 0, the item should be removed from the items' dictionary.
+# •	__repr__() - returns a string representation in the format "{shop_name} of type {shop_type} with capacity {shop_capacity}"
+
+
 class Shop:
     def __init__(self, name, type, capacity):
         self.name = name
