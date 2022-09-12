560e26987ee15856913712f3a8430661ffe7dbed
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-12 15:56:45 +0300

    Initial commit

--- OOP/Lab and Exercises/Static and Class Methods - Lab/4_hotel_room/project/hotel.py
+++ OOP/Lab and Exercises/Static and Class Methods - Lab/4_hotel_room/project/hotel.py
@@ -1,1 +1,32 @@
 
+
+from project.room import Room
+
+
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars):
+        return cls(f"{stars} stars Hotel")
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        return room.take_room(people)
+
+    def free_room(self, room_number):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        room.free_room()
+
+
+    def status(self):
+        return f"Hotel {self.name} has {sum([r.guests for r in self.rooms])} total guests\n" \
+               f"Free rooms: {', '.join([str(r.number) for r in self.rooms if not r.is_taken])}\n" \
+               f"Taken rooms: {', '.join([str(r.number) for r in self.rooms if r.is_taken])}"
+
