d1826a5f579c98fdeb3c41d7344a9142b8dc7ff0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-20 19:43:41 +0300

    Update 3_shapes.py

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/3_shapes.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/3_shapes.py
@@ -1,1 +1,4 @@
-
+Create an abstract class Shape with abstract methods calculate_area and calculate_perimeter. 
+Create classes Circle (receives radius upon initialization) and Rectangle (receives height and width upon initialization) 
+that implement those methods (returning the result). 
+The fields of Circle and Rectangle should be private.
