4d12032b3550388fafcb92a5401f2f60fe11732f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-20 19:44:05 +0300

    Initial commit

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/3_shapes.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/3_shapes.py
@@ -1,4 +1,59 @@
-Create an abstract class Shape with abstract methods calculate_area and calculate_perimeter. 
-Create classes Circle (receives radius upon initialization) and Rectangle (receives height and width upon initialization) 
-that implement those methods (returning the result). 
-The fields of Circle and Rectangle should be private.
+# Create an abstract class Shape with abstract methods calculate_area and calculate_perimeter. 
+# Create classes Circle (receives radius upon initialization) and Rectangle (receives height and width upon initialization) 
+# that implement those methods (returning the result). 
+# The fields of Circle and Rectangle should be private.
+
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self, radius) -> None:
+        self.radius = radius
+
+    def calculate_area(self):
+        return self.radius ** 2 * pi
+
+    def calculate_perimeter(self):
+        return self.radius * 2 * pi
+
+
+class Rectangle(Shape):
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def calculate_area(self):
+        return self.height * self.width
+
+    def calculate_perimeter(self):
+        return 2 * self.height + 2 * self.width
+
+
+# Zero Tests
+if __name__ == "__main__":
+    circle = Circle(5)
+    print(circle.calculate_area())
+    print(circle.calculate_perimeter())
+
+    # 78.53981633974483
+    # 31.41592653589793
+
+    rectangle = Rectangle(10, 20)
+    print(rectangle.calculate_area())
+    print(rectangle.calculate_perimeter())
+
+    # 200
+    # 60
