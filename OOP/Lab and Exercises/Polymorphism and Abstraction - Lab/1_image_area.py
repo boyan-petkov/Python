3632c389c37087b83f146f23260caa9f005c533b
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-20 19:42:56 +0300

    Initial commit

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/1_image_area.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/1_image_area.py
@@ -1,1 +1,4 @@
-
+# Create a class called ImageArea which will store the width and the height of an image. 
+# Create a method called get_area() which will return the area of the image. 
+# We have also to implement all the magic methods for comparison of two image areas (>, >=, <, <=, ==, !=), 
+# which will compare their areas.
