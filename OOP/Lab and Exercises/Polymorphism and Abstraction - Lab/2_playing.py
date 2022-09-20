1a09ac4dc4be0cdb67b8f093e00f0641fd99284c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-20 19:44:30 +0300

    Initial commit

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/2_playing.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/2_playing.py
@@ -1,1 +1,20 @@
 # Create a function called start_playing which will receive an instance and will return its play() method.
+
+class Guitar:
+    def play(self):
+        return "Playing the guitar"
+
+
+class Children:
+    def play(self):
+        return "Children are playing"
+
+
+def start_playing(obj):
+    return obj.play()
+
+
+guitar = Guitar()
+print(start_playing(guitar))
+children = Children()
+print(start_playing(children))
