94f1be532c9b1b97964f7ac3f8e8528ff28ce1b7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-20 19:44:58 +0300

    Initial commit

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/1_image_area.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Lab/1_image_area.py
@@ -2,3 +2,43 @@
 # Create a method called get_area() which will return the area of the image. 
 # We have also to implement all the magic methods for comparison of two image areas (>, >=, <, <=, ==, !=), 
 # which will compare their areas.
+
+class ImageArea:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        if self.get_area() == other.get_area():
+            return True
+        return False
+
+    def __le__(self, other):
+        if self.get_area() <= other.get_area():
+            return True
+        return False
+
+    def __lt__(self, other):
+        if self.get_area() < other.get_area():
+            return True
+        return False
+
+    def __ne__(self, other):
+        if self.get_area() != other.get_area():
+            return True
+        return False
+
+
+a1 = ImageArea(7, 10)
+a2 = ImageArea(35, 2)
+a3 = ImageArea(8, 9)
+print(a1 == a2)
+print(a1 != a3)
+print(a1 != a2)
+print(a1 >= a3)
+print(a1 <= a2)
+# print(a1.get_area(), a3.get_area())
+print(a1 < a3)
