623ed34c616e016f95004bdf7ecad51e4a506bfb
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-23 21:08:43 +0300

    Initial commit

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Exercise/4_wild_farm/animals/birds.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Exercise/4_wild_farm/animals/birds.py
@@ -1,1 +1,17 @@
 
+from project.animals.animal import Bird
+
+
+class Owl(Bird):
+    ALLOWED_FOOD = ["Meat"]
+    WEIGHT_INCREMENTAL = 0.25
+    def make_sound(self):
+        return "Hoot Hoot"
+
+
+class Hen(Bird):
+    ALLOWED_FOOD = ["Vegetable", "Meat", "Seed", "Fruit"]
+    WEIGHT_INCREMENTAL = 0.35
+    def make_sound(self):
+        return "Cluck"
+
