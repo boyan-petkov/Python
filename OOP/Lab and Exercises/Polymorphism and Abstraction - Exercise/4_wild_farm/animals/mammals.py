c0ffc2e4ee1c9d6a072aec2032d11f24ed51efdc
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-23 21:08:59 +0300

    Initial commit

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Exercise/4_wild_farm/animals/mammals.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Exercise/4_wild_farm/animals/mammals.py
@@ -1,1 +1,35 @@
 
+from project.animals.animal import Mammal
+
+
+class Mouse(Mammal):
+    ALLOWED_FOOD = ["Fruit", "Vegetable"]
+    WEIGHT_INCREMENTAL = 0.1
+
+    def make_sound(self):
+        return "Squeak"
+
+
+
+class Dog(Mammal):
+    ALLOWED_FOOD = ["Meat"]
+    WEIGHT_INCREMENTAL = 0.40
+
+    def make_sound(self):
+        return "Woof!"
+
+
+class Cat(Mammal):
+    ALLOWED_FOOD = ["Meat", "Vegetable"]
+    WEIGHT_INCREMENTAL = 0.30
+
+    def make_sound(self):
+        return "Meow"
+
+
+class Tiger(Mammal):
+    ALLOWED_FOOD = ["Meat"]
+    WEIGHT_INCREMENTAL = 1
+
+    def make_sound(self):
+        return "ROAR!!!"
