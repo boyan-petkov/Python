29074ea307db9ebf571a0721f51ae21b15260e6c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-23 21:02:48 +0300

    added description

--- OOP/Lab and Exercises/Polymorphism and Abstraction - Exercise/3_account.py
+++ OOP/Lab and Exercises/Polymorphism and Abstraction - Exercise/3_account.py
@@ -1,5 +1,6 @@
 
-Create a single class called Account. Upon initialization, it should receive an owner (str) and a starting amount (int, optional, 0 by default). It should also have an attribute called _transactions (empty list). Create the following methods:
+Create a single class called Account. Upon initialization, it should receive an owner (str) and a starting amount (int, optional, 0 by default). 
+It should also have an attribute called _transactions (empty list). Create the following methods:
 •	handle_transaction(account: Account, transaction_amout)
 o	If the balance becomes less than zero, raise ValueError with the message "sorry cannot go in debt!" and break the transaction. 
 o	Otherwise, complete it, save it and return a message "New balance: {account_balance}"
@@ -16,4 +17,6 @@ Implement the correct magic methods so the code in the example below works prope
 •	You should iterate over an account instance and receive each transaction as a result.
 •	You should be able to reverse the order of transactions by reversing an account instance.
 •	You should be able to compare (>, <, >=, <=, ==, !=) two account instances by their balance amount.
-•	When you concatenate two accounts, you should return a new account with a name - string in the format "{first_owner}&{second_owner}" and starting amount - the sum between their two. Both their transactions should be added to the new account.
+•	When you concatenate two accounts, you should return a new account with a name - string 
+in the format "{first_owner}&{second_owner}" and starting amount - the sum between their two. 
+Both their transactions should be added to the new account.
