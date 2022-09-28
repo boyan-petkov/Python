3cd109ebd1d48e6e0adddd350b602bab9e1bf236
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:37:37 +0300

    Update 5_generator_range.py

--- OOP/Lab and Exercises/Iterators and Generators - Lab/5_generator_range.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/5_generator_range.py
@@ -1,1 +1,1 @@
-Create a generator function called genrange that receives a start and an end. It should generate all the numbers from the start to the end (inclusive).
+# Create a generator function called genrange that receives a start and an end. It should generate all the numbers from the start to the end (inclusive).
