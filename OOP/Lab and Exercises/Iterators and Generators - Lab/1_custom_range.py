ef6b51f3e5fe28b954b7489bbfd85dcf1a437b9a
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:36:41 +0300

    Update 1_custom_range.py

--- OOP/Lab and Exercises/Iterators and Generators - Lab/1_custom_range.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/1_custom_range.py
@@ -1,2 +1,3 @@
 
-Create a class called custom_range that receives start and end upon initialization. Implement the __iter__ and __next__ methods, so the iterator returns the numbers from the start to the end (inclusive).
+# Create a class called custom_range that receives start and end upon initialization. 
+# Implement the __iter__ and __next__ methods, so the iterator returns the numbers from the start to the end (inclusive).
