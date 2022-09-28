713b66c40547e43568ee97dd3aab9f455af5575e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:37:25 +0300

    Update 4_squares.py

--- OOP/Lab and Exercises/Iterators and Generators - Lab/4_squares.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/4_squares.py
@@ -1,2 +1,2 @@
 
-Create a generator function called squares that should receive a number n. It should generate the squares of all numbers from 1 to n (inclusive).
+# Create a generator function called squares that should receive a number n. It should generate the squares of all numbers from 1 to n (inclusive).
