bd383a803f85766f9b7a5ff621c9554f150cf9d2
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:37:13 +0300

    Update 3_vowels.py

--- OOP/Lab and Exercises/Iterators and Generators - Lab/3_vowels.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/3_vowels.py
@@ -1,2 +1,4 @@
 
-Create a class called vowels which should receive a stirng. Implement the __iter__ and __next__ methods, so the iterator returns only the vowels from the string.
+# Create a class called vowels which should receive a stirng.
+# Implement the __iter__ and __next__ methods, so the iterator returns only the vowels from the string.
+q
