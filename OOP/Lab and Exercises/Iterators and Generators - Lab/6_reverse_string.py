a338a8ed0ffb6933017fd3d488e391a142f0c55f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:40:22 +0300

    Initial commit

--- OOP/Lab and Exercises/Iterators and Generators - Lab/6_reverse_string.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/6_reverse_string.py
@@ -1,2 +1,13 @@
 
 # Create a generator function called reverse_text that receives a string and yield all string characters in reversed order.
+
+def reverse_text(text):
+    start = len(text) - 1
+    end = 0
+    while start >= end:
+        yield text[start]
+        start -= 1
+
+
+for char in reverse_text("step"):
+    print(char, end="")
