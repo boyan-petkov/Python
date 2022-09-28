9f801ab83389c068c153afadada7fdf097934056
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:39:45 +0300

    Initial commit

--- OOP/Lab and Exercises/Iterators and Generators - Lab/5_generator_range.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/5_generator_range.py
@@ -1,1 +1,9 @@
 # Create a generator function called genrange that receives a start and an end. It should generate all the numbers from the start to the end (inclusive).
+
+def genrange(start, end):
+    while not (start > end):
+        yield start
+        start += 1
+
+
+print(list(genrange(1, 10)))
