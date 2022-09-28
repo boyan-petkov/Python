e1267f2264c740c22d52170a9ccdc552533cc5c0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:38:37 +0300

    Initial commit

--- OOP/Lab and Exercises/Iterators and Generators - Lab/2_reverse_iter.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/2_reverse_iter.py
@@ -1,3 +1,24 @@
 
 # Create a class called reverse_iter which should receive an iterable upon initialization.
 # Implement the __iter__ and __next__ methods, so the iterator returns the items of the iterable in reversed order.
+
+class reverse_iter:
+    def __init__(self, object_list):
+        self.object_list = object_list
+        self.start = len(object_list) - 1
+        self.end = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start < self.end:
+            raise StopIteration
+        index = self.start
+        self.start -= 1
+        return self.object_list[index]
+
+
+reversed_list = reverse_iter([1, 2, 3, 4])
+for item in reversed_list:
+    print(item)
