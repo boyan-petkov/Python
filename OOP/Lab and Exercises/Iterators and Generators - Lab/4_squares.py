89c9397976d10b2c5d02bf72933bcfa8e496fa24
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-09-28 09:39:21 +0300

    Initial commit

--- OOP/Lab and Exercises/Iterators and Generators - Lab/4_squares.py
+++ OOP/Lab and Exercises/Iterators and Generators - Lab/4_squares.py
@@ -1,2 +1,10 @@
 
 # Create a generator function called squares that should receive a number n. It should generate the squares of all numbers from 1 to n (inclusive).
+
+def squares(n):
+    start = 1
+    while start <= n:
+        yield start ** 2
+        start += 1
+
+print(list(squares(5)))
