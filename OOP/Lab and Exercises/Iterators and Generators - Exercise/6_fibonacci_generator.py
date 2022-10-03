07643fa60bc05b81f89a9556624ddae12ca70893
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-03 12:52:20 +0300

    Update 6_fibonacci_generator.py

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/6_fibonacci_generator.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/6_fibonacci_generator.py
@@ -1,1 +1,3 @@
 
+# Create a generator function called fibonacci() that generates the Fibonacci numbers infinitely (starting from 0).
+# Each Fibonacci number is created by the sum of the current number with the previous.
