1fef7ffc197c98d3f40419821cbcb20e31b92902
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-03 12:51:12 +0300

    Update 3_countdown_iterator.py

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/3_countdown_iterator.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/3_countdown_iterator.py
@@ -1,1 +1,3 @@
 
+# Create a class called countdown_iterator. Upon initialization it should receive a count.
+# Implement the iterator, so it returns each number of the countdown (from count to 0 inclusive).
