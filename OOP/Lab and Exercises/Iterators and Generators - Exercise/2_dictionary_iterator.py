a009efef964d26216912137d4ef167b2f2f9dd53
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-03 12:50:56 +0300

    Update 2_dictionary_iterator.py

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/2_dictionary_iterator.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/2_dictionary_iterator.py
@@ -1,1 +1,3 @@
 
+# Create a class called dictionary_iter. Upon initialization it should receive a dictionary object. 
+# Implement the iterator, so it returns each key-value pair of the dictionary as a tuple of two elements (the key and the value).
