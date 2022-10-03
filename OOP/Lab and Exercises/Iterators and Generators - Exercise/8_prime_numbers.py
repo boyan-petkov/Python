368bb6608413ffa33140c62d6837d970df3f9763
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-03 12:53:12 +0300

    Update 8_prime_numbers.py

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/8_prime_numbers.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/8_prime_numbers.py
@@ -1,1 +1,3 @@
 
+# Create a generator function called get_primes() which should receive a list of integer numbers and return a list containing only the prime numbers from the initial list. 
+# You can learn more about prime numbers from here
