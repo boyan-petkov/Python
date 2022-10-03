8b12ff0f1d39cfe3e520a873d96c7d1624299ba4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-03 12:51:53 +0300

    Update 5_take_halves.py

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/5_take_halves.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/5_take_halves.py
@@ -1,1 +1,8 @@
 
+You are given a skeleton with the following code:
+ 
+Implement the three generator functions:
+•	integers() - generates an infinite amount of integers (starting from 1)
+•	halves() - generates the halves of those integers (each integer / 2)
+•	take(n, seq) - takes the first n halves of those integers
+Note: Complete the functionality in the skeleton and submit it in the judge system
