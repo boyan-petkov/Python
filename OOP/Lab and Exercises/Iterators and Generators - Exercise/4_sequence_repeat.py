cb82cd7bae5a8017b4b2e007ca6805fffb114110
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-03 12:51:30 +0300

    Update 4_sequence_repeat.py

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/4_sequence_repeat.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/4_sequence_repeat.py
@@ -1,1 +1,4 @@
 
+# Create a class called sequence_repeat which should receive a sequence and a number upon initialization. 
+# Implement an iterator to return the given elements the defined number of times. 
+# If the number is greater than the number of elements, then the sequence repeats as necessary.
