a94b2d55042cbb70db04dad96e6d212b54cf40fa
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-03 12:52:39 +0300

    Update 7_reader.py

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/7_reader.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/7_reader.py
@@ -1,1 +1,3 @@
 
+# Create a generator function called read_next() which should receive different number of arguments (all iterable).
+# On each iteration it should return the next element from the current iterable, or the first element from the subsequent iterable.
