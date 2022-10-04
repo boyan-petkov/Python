afba623140e4db4df470c6ec30216cdb055095d2
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-04 10:51:59 +0300

    Initial commit

--- OOP/Lab and Exercises/Iterators and Generators - Exercise/5_take_halves.py
+++ OOP/Lab and Exercises/Iterators and Generators - Exercise/5_take_halves.py
@@ -6,3 +6,26 @@
 # •	halves() - generates the halves of those integers (each integer / 2)
 # •	take(n, seq) - takes the first n halves of those integers
 # Note: Complete the functionality in the skeleton and submit it in the judge system
+
+
+def solution():
+    def integers():
+        i = 0
+        while True:
+            i += 1
+            yield i
+
+    def halves():
+        for i in integers():
+            yield i / 2
+
+    def take(n, seq):
+        return [next(seq) for _ in range(n)]
+
+    return take, halves, integers
+
+
+if __name__ == '__main__':
+    take = solution()[0]
+    halves = solution()[1]
+    print(take(5, halves()))
