0a8a63e11d924a9cc424959ebf63573a0a6084d4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-06 17:24:26 +0300

    Update 3_even_numbers.py

--- OOP/Lab and Exercises/Decorators - Lab/3_even_numbers.py
+++ OOP/Lab and Exercises/Decorators - Lab/3_even_numbers.py
@@ -1,1 +1,12 @@
 
+Having the following code:
+
+def even_numbers(function):
+
+    def wrapper(numbers):
+
+        # TODO: Implement
+
+    return wrapper
+
+Complete the code so it works as expected
