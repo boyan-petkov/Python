5a7be89db6f1dfcea236c2a63ed2e3fb2450ffcc
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-06 17:28:06 +0300

    Initial commit

--- OOP/Lab and Exercises/Decorators - Lab/2_vowel_filter.py
+++ OOP/Lab and Exercises/Decorators - Lab/2_vowel_filter.py
@@ -10,3 +10,17 @@
 #     return wrapper
 
 # Complete the code so it works as expected
+
+
+def vowel_filter(function):
+    def wrapper():
+        all_letters = function()
+        vowels = "aeiuoy"
+        res = [l for l in all_letters if l in vowels]
+        return res
+    return wrapper
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
