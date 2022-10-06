e7f0f778a72bdd8543f26e39adbc3f317160cba7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-06 17:29:04 +0300

    Initial commit

--- OOP/Lab and Exercises/Decorators - Lab/4_multiply.py
+++ OOP/Lab and Exercises/Decorators - Lab/4_multiply.py
@@ -18,3 +18,17 @@
 # print(add_ten(3))
 
 
+def multiply(n):
+    def decorator(function):
+        def wrapper(number):
+            temp = function(number)
+            res = temp * n
+            return res
+        return wrapper
+    return decorator
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
+
+print(add_ten(3))
