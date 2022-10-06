4664ee50bb0f75115bff82a67cf5183d0c96723f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-06 17:27:39 +0300

    Initial commit

--- OOP/Lab and Exercises/Decorators - Lab/1_number_increment.py
+++ OOP/Lab and Exercises/Decorators - Lab/1_number_increment.py
@@ -13,3 +13,11 @@
 
 # print(number_increment([1, 2, 3]))	[2, 3, 4]
 
+def number_increment(nums):
+    def increase():
+        res = [n + 1 for n in nums]
+        return res
+    return increase()
+
+
+print(number_increment([1, 2, 3]))
