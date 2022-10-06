8ec10f07757f1fe1cea36f199250f0412e1e93a3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-06 17:22:51 +0300

    Update 1_number_increment.py

--- OOP/Lab and Exercises/Decorators - Lab/1_number_increment.py
+++ OOP/Lab and Exercises/Decorators - Lab/1_number_increment.py
@@ -1,1 +1,14 @@
+# Having the following code:
 
+
+# def number_increment(numbers):
+
+#     def increase():
+
+#         # TODO: Implement
+
+#     return increase()
+  
+# Complete the code so it works as expected
+
+# print(number_increment([1, 2, 3]))	[2, 3, 4]
