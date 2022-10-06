9564233eec0b7d4b96ed233d5dbaf106e9475ea9
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-06 17:26:22 +0300

    Update 4_multiply.py

--- OOP/Lab and Exercises/Decorators - Lab/4_multiply.py
+++ OOP/Lab and Exercises/Decorators - Lab/4_multiply.py
@@ -1,1 +1,20 @@
 
+Having the following code:
+  def multiply(times):
+
+    def decorator(function):
+
+        # TODO: Implement
+
+    return decorator
+  
+Complete the code so it works as expected
+
+Test Code                      Output
+@multiply(3)                   39
+def add_ten(number):
+    return number + 10
+
+print(add_ten(3))
+
+
