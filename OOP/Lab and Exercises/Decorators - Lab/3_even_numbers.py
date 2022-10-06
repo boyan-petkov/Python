26c80e8e56e708d1664722fd24b7362701a5b6b1
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-06 17:25:17 +0300

    Update 3_even_numbers.py

--- OOP/Lab and Exercises/Decorators - Lab/3_even_numbers.py
+++ OOP/Lab and Exercises/Decorators - Lab/3_even_numbers.py
@@ -10,3 +10,10 @@ def even_numbers(function):
     return wrapper
 
 Complete the code so it works as expected
+ 
+Input                                                 Output
+@even_numbers                                          [2, 4]
+def get_numbers(numbers):
+    return numbers
+print(get_numbers([1, 2, 3, 4, 5]))      
+
