1c48713285edc26041b143d15d6dbe869b1ab9cc
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-11 14:15:01 +0300

    Update 1_logged.py

--- OOP/Lab and Exercises/Decorators - Exercise/1_logged.py
+++ OOP/Lab and Exercises/Decorators - Exercise/1_logged.py
@@ -1,1 +1,2 @@
 
+Create a decorator called logged. It should return the name of the function that is being called and its parameters. It should also return the result of the execution of the function being called. See the examples for more clarification.
