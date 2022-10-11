7ee5d402f2390e78629fc33b7c02f43e58df6e12
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-11 14:17:31 +0300

    Update 3_bold_italc_underline.py

--- OOP/Lab and Exercises/Decorators - Exercise/3_bold_italc_underline.py
+++ OOP/Lab and Exercises/Decorators - Exercise/3_bold_italc_underline.py
@@ -1,2 +1,3 @@
 
-Create three decorators: make_bold, make_italic, make_underline, which will have to wrap a text returned from a function in <b></b>, <i></i> and <u></u> respectively.
+# Create three decorators: make_bold, make_italic, make_underline, 
+#   which will have to wrap a text returned from a function in <b></b>, <i></i> and <u></u> respectively.
