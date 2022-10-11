d5de230f671574a1744132c80b691ebd57be3fcb
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-11 14:16:07 +0300

    Update 5_cache.py

--- OOP/Lab and Exercises/Decorators - Exercise/5_cache.py
+++ OOP/Lab and Exercises/Decorators - Exercise/5_cache.py
@@ -1,2 +1,3 @@
 
 Create a decorator called cache. It should store all the returned values of the recursive function fibonacci. You are provided with this code:
+You need to create a dictionary called log that will store all the n's (keys) and the returned results (values) and attach that dictionary to the fibonacci function as a variable called log, so when you call it, it returns that dictionary. For more clarification, see the examples
