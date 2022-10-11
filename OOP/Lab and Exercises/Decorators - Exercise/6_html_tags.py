20cf6259464507d006fa5397f5822c178abe83e0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-11 14:18:26 +0300

    Update 6_html_tags.py

--- OOP/Lab and Exercises/Decorators - Exercise/6_html_tags.py
+++ OOP/Lab and Exercises/Decorators - Exercise/6_html_tags.py
@@ -1,2 +1,4 @@
 
-Create a decorator called tags. It should receive an HTML tag as a parameter, wrap the result of a function with the given tag and return the new result. For more clarification, see the examples below
+# Create a decorator called tags. 
+# It should receive an HTML tag as a parameter, wrap the result of a function with the given tag and return the new result. 
+# For more clarification, see the examples below
