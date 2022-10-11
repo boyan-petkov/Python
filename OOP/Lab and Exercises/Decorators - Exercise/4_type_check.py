88690c7943cebe4b4fed9934d43ed6ee68d018f8
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-11 14:15:42 +0300

    Update 4_type_check.py

--- OOP/Lab and Exercises/Decorators - Exercise/4_type_check.py
+++ OOP/Lab and Exercises/Decorators - Exercise/4_type_check.py
@@ -1,1 +1,2 @@
 
+Create a decorator called type_check. It should receive a type (int/float/str/…), and it should check if the parameter passed to the decorated function is of the type given to the decorator. If it is, execute the function and return the result, otherwise return "Bad Type".
