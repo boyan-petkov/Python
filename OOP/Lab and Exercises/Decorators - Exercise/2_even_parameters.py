66e949b4f19f83a1a0ece6dd431ff9ed5fc2453f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-11 14:15:12 +0300

    Update 2_even_parameters.py

--- OOP/Lab and Exercises/Decorators - Exercise/2_even_parameters.py
+++ OOP/Lab and Exercises/Decorators - Exercise/2_even_parameters.py
@@ -1,1 +1,2 @@
 
+Create a decorator function called even_parameters. It should check if all parameters passed to a function are even numbers and only then execute the function and return the result. Otherwise, don't execute the function and return "Please use only even numbers!"
