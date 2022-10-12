ab067e979817ffadb12efa0e62a482f73b572960
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-12 12:39:56 +0300

    Update 3_bold_italc_underline.py

--- OOP/Lab and Exercises/Decorators - Exercise/3_bold_italc_underline.py
+++ OOP/Lab and Exercises/Decorators - Exercise/3_bold_italc_underline.py
@@ -1,3 +1,41 @@
 
 # Create three decorators: make_bold, make_italic, make_underline, 
 #   which will have to wrap a text returned from a function in <b></b>, <i></i> and <u></u> respectively.
+def make_bold(func):
+    def wrapper(*args, **kwargs):
+        retval = f'<b>{func(*args, **kwargs)}</b>'
+        return retval
+    return wrapper
+
+
+def make_italic(func):
+    def wrapper(*args, **kwargs):
+        retval = f'<i>{func(*args, **kwargs)}</i>'
+        return retval
+    return wrapper
+
+
+def make_underline(func):
+    def wrapper(*args, **kwargs):
+        retval = f'<u>{func(*args, **kwargs)}</u>'
+        return retval
+    return wrapper
+
+
+@make_bold
+@make_italic
+@make_underline
+def greet(name):
+    return f"Hello, {name}"
+
+
+print(greet("Peter"))
+
+
+@make_bold
+@make_italic
+@make_underline
+def greet_all(*args):
+    return f"Hello, {', '.join(args)}"
+
+print(greet_all("Peter", "George"))
