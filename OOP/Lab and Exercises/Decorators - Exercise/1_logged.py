909e9ba61cb720da4b70d5aadc27c4ebca6af3d9
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-12 12:39:15 +0300

    Initial commit

--- OOP/Lab and Exercises/Decorators - Exercise/1_logged.py
+++ OOP/Lab and Exercises/Decorators - Exercise/1_logged.py
@@ -1,3 +1,24 @@
 
 # Create a decorator called logged. It should return the name of the function that is being called and its parameters. 
 # It should also return the result of the execution of the function being called. See the examples for more clarification.
+
+def logged(function):
+    def wrapper(*args):
+        retval = f'you called {function.__name__}{args}\n'
+        retval += f'it returned {function(*args)}'
+        return retval
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+
+
+@logged
+def sum_func(a, b):
+    return a + b
+
+
+print(func(4, 4, 4))
+print(sum_func(1, 4))
