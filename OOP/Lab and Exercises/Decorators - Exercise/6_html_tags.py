58859d1ea3a7fc2e4c82b56040af02df8e0f51c3
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-12 12:54:22 +0300

    Initial commit

--- OOP/Lab and Exercises/Decorators - Exercise/6_html_tags.py
+++ OOP/Lab and Exercises/Decorators - Exercise/6_html_tags.py
@@ -1,7 +1,6 @@
-
 # Create a decorator called tags. 
 # It should receive an HTML tag as a parameter, wrap the result of a function with the given tag and return the new result. 
-# For more clarification, see the examples below
+
 def tags(tag):
     def inner(func):
         def wrapper(*args, **kwargs):
