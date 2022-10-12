08e919f43f0bc2b87508863a1dd112cde578f103
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-12 12:53:00 +0300

    Initial commit

--- OOP/Lab and Exercises/Decorators - Exercise/3_bold_italc_underline.py
+++ OOP/Lab and Exercises/Decorators - Exercise/3_bold_italc_underline.py
@@ -1,6 +1,6 @@
-
 # Create three decorators: make_bold, make_italic, make_underline, 
 #   which will have to wrap a text returned from a function in <b></b>, <i></i> and <u></u> respectively.
+
 def make_bold(func):
     def wrapper(*args, **kwargs):
         retval = f'<b>{func(*args, **kwargs)}</b>'
