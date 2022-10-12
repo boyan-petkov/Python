6b3bd7c0b5452bb2213a9814aaeda24fddbcbb41
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-12 12:52:27 +0300

    Initial commit

--- OOP/Lab and Exercises/Decorators - Exercise/1_logged.py
+++ OOP/Lab and Exercises/Decorators - Exercise/1_logged.py
@@ -1,4 +1,3 @@
-
 # Create a decorator called logged. It should return the name of the function that is being called and its parameters. 
 # It should also return the result of the execution of the function being called. See the examples for more clarification.
 
