a1616bcf2d8fc1c6e0c7cb31cf4e5d236e928eae
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-17 11:29:36 +0300

    Update 3_list.py

--- OOP/Lab and Exercises/Testing - Lab/3_list.py
+++ OOP/Lab and Exercises/Testing - Lab/3_list.py
@@ -1,18 +1,20 @@
-You are provided with a class IntegerList. It should only store integers. The initial integers should be set by constructor. They are stored as a list. IntegerList has a functionality to add, remove_index, get, insert, get the biggest number and get the index of an element. Your task is to test the class.
-Note: You are not allowed to change the structure of the provided code
+# You are provided with a class IntegerList. It should only store integers. The initial integers should be set by constructor.
+# They are stored as a list. IntegerList has a functionality to add, remove_index, get, insert, get the biggest number and get the index of an element. 
+# Your task is to test the class.
+# Note: You are not allowed to change the structure of the provided code
 
-Constraints
-•	add operation, should add an element and returns the list.
-o	If the element is not an integer, a ValueError is thrown
-•	remove_index operation removes the element on that index and returns it.
-o	If the index is out of range, an IndexError is thrown
-•	__init__ should only take integers, and store them
-•	get should return the specific element
-o	If the index is out of range, an IndexError is thrown
-•	insert
-o	If the index is out of range, IndexError is thrown
-o	If the element is not an integer, ValueError is thrown
-•	get_biggest
-•	get_index
-Hint
-Do not forget to test the constructor
+# Constraints
+# •	add operation, should add an element and returns the list.
+# o	If the element is not an integer, a ValueError is thrown
+# •	remove_index operation removes the element on that index and returns it.
+# o	If the index is out of range, an IndexError is thrown
+# •	__init__ should only take integers, and store them
+# •	get should return the specific element
+# o	If the index is out of range, an IndexError is thrown
+# •	insert
+# o	If the index is out of range, IndexError is thrown
+# o	If the element is not an integer, ValueError is thrown
+# •	get_biggest
+# •	get_index
+# Hint
+# Do not forget to test the constructor
