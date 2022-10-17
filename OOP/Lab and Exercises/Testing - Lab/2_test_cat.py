e0b654ce624da0006231e0e83d50f6e4894ba500
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-17 11:28:36 +0300

    Update 2_test_cat.py

--- OOP/Lab and Exercises/Testing - Lab/2_test_cat.py
+++ OOP/Lab and Exercises/Testing - Lab/2_test_cat.py
@@ -19,3 +19,15 @@
 #       raise Exception('Cannot sleep while hungry')
 
 #     self.sleepy = False
+
+
+Create a class CatTests
+In judge you need to submit just the CatTests class, with the unitttest module imported.
+Create the following tests:
+•	Cat's size is increased after eating
+•	Cat is fed after eating
+•	Cat cannot eat if already fed, raises an error
+•	Cat cannot fall asleep if not fed, raises an error
+•	Cat is not sleepy after sleeping
+Hints
+Follow the logic of the previous problem
