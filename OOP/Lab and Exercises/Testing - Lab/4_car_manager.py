abf794362bf6333623e5365c2ad5219b4dafa388
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-17 11:30:38 +0300

    Update 4_car_manager.py

--- OOP/Lab and Exercises/Testing - Lab/4_car_manager.py
+++ OOP/Lab and Exercises/Testing - Lab/4_car_manager.py
@@ -1,11 +1,15 @@
-You are provided with a simple project containing only one class - Car. The provided class is simple - its main point is to represent some of the functionality of a Car. Each car contains information about its make, model, fuel consumption, fuel amount and fuel capacity. Also, each Car can add some fuel to its tank by refueling and can travel distance by driving. In order to be driven, our Car needs to have enough fuel. Everything in the provided skeleton is working perfectly fine and you mustn't change it.
-Your job now is to write unit tests on the provided project and its functionality. You should test every part of code inside the Car class:
-•	You should test the constructor
-•	You should test all the methods and validations inside the class
-Constraints
-•	Everything in the provided skeleton is working perfectly fine
-•	You mustn't change anything in the project structure
-•	Any part of validation should be tested
-•	There is no limit on the tests you can write but keep your attention on the main functionality
-Note: You are not allowed to change the structure of the provided code
+# You are provided with a simple project containing only one class - Car. 
+# The provided class is simple - its main point is to represent some of the functionality of a Car. 
+# Each car contains information about its make, model, fuel consumption, fuel amount and fuel capacity.
+# Also, each Car can add some fuel to its tank by refueling and can travel distance by driving. In order to be driven, our Car needs to have enough fuel. 
+# Everything in the provided skeleton is working perfectly fine and you mustn't change it.
+# Your job now is to write unit tests on the provided project and its functionality. You should test every part of code inside the Car class:
+# •	You should test the constructor
+# •	You should test all the methods and validations inside the class
+# Constraints
+# •	Everything in the provided skeleton is working perfectly fine
+# •	You mustn't change anything in the project structure
+# •	Any part of validation should be tested
+# •	There is no limit on the tests you can write but keep your attention on the main functionality
+# Note: You are not allowed to change the structure of the provided code
 
