919e64321fe53cc57ba130381f000c135b48d830
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-18 10:25:16 +0300

    Initial commit

--- OOP/Lab and Exercises/Testing - Lab/2_test_cat.py
+++ OOP/Lab and Exercises/Testing - Lab/2_test_cat.py
@@ -31,3 +31,48 @@
 # •	Cat is not sleepy after sleeping
 # Hints
 # Follow the logic of the previous problem
+
+from unittest import TestCase, main
+class CatTests(TestCase):
+    def test_if_correctly_initialized(self):
+        cat = Cat("Leon")
+        self.assertEqual("Leon", cat.name)
+        self.assertFalse(cat.fed)
+        self.assertFalse(cat.sleepy)
+        self.assertEqual(0, cat.size)
+
+    def test_if_cat_size_increased_after_eating(self):
+        cat = Cat("Leon")
+        cat.eat()
+        self.assertEqual(1, cat.size)
+
+    def test_cat_attributes_after_eating(self):
+        cat = Cat("Leon")
+        cat.eat()
+        self.assertTrue(cat.fed)
+        self.assertTrue(cat.sleepy)
+
+    def test_if_error_is_raises_with_eat_method_after_eating(self):
+        with self.assertRaises(Exception) as error:
+            cat = Cat("Leon")
+            cat.eat()
+            cat.eat()
+        expected_msg = 'Already fed.'
+        self.assertEqual(expected_msg, str(error.exception))
+
+    def test_if_try_sleep_hungry_exception_raised(self):
+        with self.assertRaises(Exception) as error:
+            cat = Cat("Leon")
+            cat.sleep()
+        expected = 'Cannot sleep while hungry'
+        self.assertEqual(expected, str(error.exception))
+
+    def test_if_cat_sleepy_after_sleep(self):
+        cat = Cat("Leon")
+        cat.eat()
+        cat.sleep()
+        self.assertFalse(cat.sleepy)
+
+
+if __name__ == "__main__":
+    main()
