5fb6a6a09fedf9e0a921f792450970be4151df0d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-18 10:23:38 +0300

    Update 1_test_worker.py

--- OOP/Lab and Exercises/Testing - Lab/1_test_worker.py
+++ OOP/Lab and Exercises/Testing - Lab/1_test_worker.py
@@ -19,12 +19,12 @@
 #   def get_info(self):
 #     return (f'{self.name} has saved {self.money} money.')
 
-Create a class WorkerTests
-In judge you need to submit just the WokerTests class, with the unitttest module imported.
-Create the following tests:
-•	Test if the worker is initialized with correct name, salary and energy
-•	Test if the worker's energy is incremented after the rest method is called
-•	Test if an error is raised if the worker tries to work with negative energy or equal to 0
-•	Test if the worker's money is increased by his salary correctly after the work method is called
-•	Test if the worker's energy is decreased after the work method is called	
-•	Test if the get_info method returns the proper string with correct values
+# Create a class WorkerTests
+# In judge you need to submit just the WokerTests class, with the unitttest module imported.
+# Create the following tests:
+# •	Test if the worker is initialized with correct name, salary and energy
+# •	Test if the worker's energy is incremented after the rest method is called
+# •	Test if an error is raised if the worker tries to work with negative energy or equal to 0
+# •	Test if the worker's money is increased by his salary correctly after the work method is called
+# •	Test if the worker's energy is decreased after the work method is called	
+# •	Test if the get_info method returns the proper string with correct values
