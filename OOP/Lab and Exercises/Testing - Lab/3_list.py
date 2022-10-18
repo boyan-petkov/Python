29d73f2ab9f03f7104e5c7727aab11a5a35aec83
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-18 10:24:05 +0300

    Update 3_list.py

--- OOP/Lab and Exercises/Testing - Lab/3_list.py
+++ OOP/Lab and Exercises/Testing - Lab/3_list.py
@@ -1,45 +1,45 @@
-class IntegerList:
-    def __init__(self, *args):
-        self.__data = []
-        for x in args:
-            if type(x) == int:
-                self.__data.append(x)
+# class IntegerList:
+#     def __init__(self, *args):
+#         self.__data = []
+#         for x in args:
+#             if type(x) == int:
+#                 self.__data.append(x)
  
-    def get_data(self):
-        return self.__data
+#     def get_data(self):
+#         return self.__data
  
-    def add(self, element):
-        if not type(element) == int:
-            raise ValueError("Element is not Integer")
-        self.get_data().append(element)
-        return self.get_data()
+#     def add(self, element):
+#         if not type(element) == int:
+#             raise ValueError("Element is not Integer")
+#         self.get_data().append(element)
+#         return self.get_data()
  
-    def remove_index(self, index):
-        if index >= len(self.get_data()):
-            raise IndexError("Index is out of range")
-        a = self.get_data()[index]
-        del self.get_data()[index]
-        return a
+#     def remove_index(self, index):
+#         if index >= len(self.get_data()):
+#             raise IndexError("Index is out of range")
+#         a = self.get_data()[index]
+#         del self.get_data()[index]
+#         return a
  
-    def get(self, index):
-        if index >= len(self.get_data()):
-            raise IndexError("Index is out of range")
-        return self.get_data()[index]
+#     def get(self, index):
+#         if index >= len(self.get_data()):
+#             raise IndexError("Index is out of range")
+#         return self.get_data()[index]
  
-    def insert(self, index, el):
-        if index >= len(self.get_data()):
-            raise IndexError("Index is out of range")
-        elif not type(el) == int:
-            raise ValueError("Element is not Integer")
+#     def insert(self, index, el):
+#         if index >= len(self.get_data()):
+#             raise IndexError("Index is out of range")
+#         elif not type(el) == int:
+#             raise ValueError("Element is not Integer")
  
-        self.get_data().insert(index, el)
+#         self.get_data().insert(index, el)
  
-    def get_biggest(self):
-        a = sorted(self.get_data(), reverse=True)
-        return a[0]
+#     def get_biggest(self):
+#         a = sorted(self.get_data(), reverse=True)
+#         return a[0]
  
-    def get_index(self, el):
-        return self.get_data().index(el)
+#     def get_index(self, el):
+#         return self.get_data().index(el)
     
     
 # You are provided with a class IntegerList. It should only store integers. The initial integers should be set by constructor.
