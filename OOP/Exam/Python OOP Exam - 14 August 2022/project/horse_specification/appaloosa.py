7be197cb62594048f407275558cc41cc58b3fa38
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-31 09:17:38 +0200

    Initial commit

--- OOP/Exam/Python OOP Exam - 14 August 2022/project/horse_specification/appaloosa.py
+++ OOP/Exam/Python OOP Exam - 14 August 2022/project/horse_specification/appaloosa.py
@@ -1,1 +1,20 @@
 
+from project.horse_specification.horse import Horse
+
+
+class Appaloosa(Horse):
+
+    def __init__(self, name: str, speed: int):
+        super().__init__(name, speed)
+
+    def maximum_speed(self):
+        return 120
+
+    def train(self):
+        current = self.speed
+        maximum = self.maximum_speed()
+        train_points = 2
+        if current + train_points > maximum:
+            self.speed = maximum
+        else:
+            self.speed += train_points
