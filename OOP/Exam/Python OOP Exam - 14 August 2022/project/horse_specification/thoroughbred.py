4ad4275a378b344587cdb66983851e54d82b591c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-31 09:18:35 +0200

    Initial commit

--- OOP/Exam/Python OOP Exam - 14 August 2022/project/horse_specification/thoroughbred.py
+++ OOP/Exam/Python OOP Exam - 14 August 2022/project/horse_specification/thoroughbred.py
@@ -1,1 +1,20 @@
+from project.horse_specification.horse import Horse
+
+
+class Thoroughbred(Horse):
+
+    def __init__(self, name: str, speed: int):
+        super().__init__(name, speed)
+
+    def maximum_speed(self):
+        return 140
+
+    def train(self):
+        current = self.speed
+        maximum = self.maximum_speed()
+        train_points = 3
+        if current + train_points > maximum:
+            self.speed = maximum
+        else:
+            self.speed += train_points
 
