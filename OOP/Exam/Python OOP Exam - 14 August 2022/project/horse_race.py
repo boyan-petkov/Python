0b038e8c536ab9aa9912cd7533dab4eab3ddb916
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2022-10-31 08:58:44 +0200

    Initial commit

--- OOP/Exam/Python OOP Exam - 14 August 2022/project/horse_race.py
+++ OOP/Exam/Python OOP Exam - 14 August 2022/project/horse_race.py
@@ -1,1 +1,21 @@
 
+class HorseRace:
+    __VALID_RACE_TYPES = ["Winter", "Spring", "Autumn", "Summer"]
+    __INVALID_RACE_TYPE_MSG = "Race type does not exist!"
+
+    def __init__(self, race_type: str):
+        self.race_type = race_type
+        self.jockeys = []
+
+    def __validate_race_type(self, value):
+        if value not in self.__VALID_RACE_TYPES:
+            raise ValueError(self.__INVALID_RACE_TYPE_MSG)
+
+    @property
+    def race_type(self):
+        return self.__race_type
+
+    @race_type.setter
+    def race_type(self, value):
+        self.__validate_race_type(value)
+        self.__race_type = value
